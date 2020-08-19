5772c278adf8690b1bf75224767efe2baaf478ce
Michael Yankelev <myankelev@gmail.com>
2020-08-20 00:57:25 +0200

    fix imports

--- packages/files-ui/src/App.tsx
+++ packages/files-ui/src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react"
-import * as Sentry from "@sentry/react"
+import { init, ErrorBoundary } from "@sentry/react"
 
 if (
-  // process.env.NODE_ENV === "production" &&
+  process.env.NODE_ENV === "production" &&
   process.env.REACT_APP_SENTRY_RELEASE &&
   process.env.REACT_APP_SENTRY_DSN_URL
 ) {
-  Sentry.init({
+  init({
     dsn: process.env.REACT_APP_SENTRY_DSN_URL,
     release: process.env.REACT_APP_SENTRY_RELEASE,
     environment: process.env.REACT_APP_SENTRY_ENV,
@@ -17,9 +17,9 @@ if (
 
 const App: React.FC<{}> = () => {
   return (
-    <Sentry.ErrorBoundary showDialog fallback={"there was an error"}>
+    <ErrorBoundary showDialog>
       <div className="App"></div>
-    </Sentry.ErrorBoundary>
+    </ErrorBoundary>
   )
 }
 
