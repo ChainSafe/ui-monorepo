46b55816d16f55a40099b7639d8d9658f64e48fa
Michael Yankelev <myankelev@gmail.com>
2020-08-20 11:18:43 +0200

    wire up Error Boundary

--- packages/files-ui/src/App.tsx
+++ packages/files-ui/src/App.tsx
@@ -1,26 +1,41 @@
-import React from "react"
-import { init, ErrorBoundary } from "@sentry/react"
+import React, { useState } from "react"
+import { init, ErrorBoundary, showReportDialog } from "@sentry/react"
 
 if (
-  process.env.NODE_ENV === "production" &&
-  process.env.REACT_APP_SENTRY_RELEASE &&
-  process.env.REACT_APP_SENTRY_DSN_URL
+  // process.env.NODE_ENV === "production" &&
+  process.env.REACT_APP_SENTRY_DSN_URL &&
+  process.env.REACT_APP_SENTRY_RELEASE
 ) {
   init({
     dsn: process.env.REACT_APP_SENTRY_DSN_URL,
     release: process.env.REACT_APP_SENTRY_RELEASE,
-    environment: process.env.REACT_APP_SENTRY_ENV,
   })
 }
 // Add router
 // Add all global contexts (theme, language, apis, web3)
 
-const App: React.FC<{}> = () => {
-  return (
-    <ErrorBoundary showDialog>
-      <div className="App"></div>
-    </ErrorBoundary>
-  )
-}
+const App: React.FC<{}> = () => (
+  <ErrorBoundary
+    fallback={({ error, componentStack, eventId, resetError }) => (
+      <div>
+        <p>
+          An error occured and has been logged. If you would like to provide
+          additional info to help us debug and resolve the issue, click the
+          "Provide Additional Details" button
+        </p>
+        <p>{error?.message.toString()}</p>
+        <p>{componentStack}</p>
+        <p>{eventId}</p>
+        <button onClick={() => showReportDialog({ eventId: eventId || "" })}>
+          Provide Additional Details
+        </button>
+        <button onClick={resetError}>Reset error</button>
+      </div>
+    )}
+    onReset={() => window.location.reload()}
+  >
+    <div className="App"></div>
+  </ErrorBoundary>
+)
 
 export default App
