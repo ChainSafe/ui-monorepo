dfd7b66a80e81b81d9b581012ec279aaba7d98ea
Michael Yankelev <myankelev@gmail.com>
2020-08-21 12:32:31 +0200

    [WIP] add dropzone and formik

---
+++ packages/common-components/src/FileInput/FormikDropzoneControl.tsx
@@ -0,0 +1,94 @@
+/**
+ *
+ * FormikDropzoneControl
+ *
+ */
+
+import React, { Fragment } from "react"
+import { makeStyles, createStyles } from "@material-ui/styles"
+import Dropzone from "react-dropzone"
+// import UploadedPdfIcon from "components/UploadedPdfIcon"
+import { FieldProps, getIn } from "formik"
+
+const useStyles = makeStyles((theme: ITheme) =>
+  createStyles({
+    dropzone: {
+      "&> *": {
+        width: "100%",
+        height: "auto",
+        borderWidth: 2,
+        borderColor: "rgb(102, 102, 102)",
+        borderStyle: "dashed",
+        borderRadius: 5,
+      },
+    },
+  }),
+)
+
+interface OwnProps extends FieldProps {
+  setFieldValue(fieldName: string, newFiles: any): void
+}
+
+const FormikDropzoneControl: React.SFC<OwnProps> = ({
+  field,
+  form,
+}: OwnProps) => {
+  if (!Array.isArray(field.value)) {
+    console.log("Input field provided to Dropzone is not an array")
+    return <Fragment />
+  }
+  const classes = useStyles()
+  const { name: fieldName, value: fieldValue } = field
+  const { touched, errors, setFieldValue, setFieldTouched, dirty } = form
+
+  const fieldError = getIn(errors, fieldName)
+  const showError = getIn(touched, fieldName) && !!fieldError
+
+  return (
+    <Fragment>
+      <div className={classes.dropzone}>
+        <Dropzone
+          accept="application/pdf"
+          disableClick={true}
+          style={{
+            display: "flex",
+          }}
+          onDrop={acceptedFiles => {
+            if (acceptedFiles.length === 0) {
+              return
+            }
+            setFieldValue(fieldName, fieldValue.concat(acceptedFiles))
+            setFieldTouched(fieldName, true)
+          }}
+        >
+          {({ isDragActive, isDragReject }) => {
+            if (isDragActive) {
+              return "This file is authorized"
+            }
+            if (isDragReject) {
+              return "This file is not authorized"
+            }
+            if (fieldValue.length === 0) {
+              return <p>Drag documents here</p>
+            }
+            return fieldValue.map((file, i) => (
+              <UploadedPdfIcon
+                key={i}
+                file={file}
+                onDelete={() => {
+                  setFieldValue(
+                    name,
+                    fieldValue.filter((tag, index) => index !== i),
+                  )
+                }}
+              />
+            ))
+          }}
+        </Dropzone>
+      </div>
+      {showError && <FormHelperText error>{fieldError}</FormHelperText>}
+    </Fragment>
+  )
+}
+
+export default withStyles(styles, { withTheme: true })(FormikDropzoneControl)
