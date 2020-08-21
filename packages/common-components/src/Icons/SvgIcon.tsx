2bd321a9cf49bb4a8cb6db33aad771c336c9a6f5
Tanmoy Basak Anjan <tanmoy3399@gmail.com>
2020-08-21 21:53:02 +0600

    Icons (#40)
    * Wrapping up type declarations
    
    * Switch case swapped out pending test
    
    * generalize onclick
    
    * Theme provider not working
    
    * added svgIcon component
    
    * icons stories ready
    
    * theme trial
    
    * added custom Icon
    
    * added custom Icon
    
    * exporting icons from files
    
    * removed redundant code
    
    * using px
    
    * using rollup plugins
    
    * typography exportt warning
    
    * remove extra props from story
    
    * updated theme variables
    
    Co-authored-by: RyRy79261 <ryanjnoble@gmail.com>
    Co-authored-by: Michael Yankelev <myankelev@gmail.com>

---
+++ packages/common-components/src/Icons/SvgIcon.tsx
@@ -0,0 +1,81 @@
+import React, { ReactNode } from "react"
+import clsx from "clsx"
+import { makeStyles, createStyles } from "@material-ui/styles"
+import { ITheme } from "@chainsafe/common-themes"
+import { capitalize } from "../utils/stringUtils"
+
+const useStyles = makeStyles((theme: ITheme) =>
+  createStyles({
+    root: {
+      userSelect: "none",
+      width: "1em",
+      height: "1em",
+      display: "inline-block",
+      fontSize: "1.5em",
+      transition: `all ${theme.animation.transform}ms`,
+    },
+    colorPrimary: {
+      fill: theme.palette.primary.main,
+    },
+    colorSecondary: {
+      fill: theme.palette.secondary ? theme.palette.secondary.main : "inherit",
+    },
+    colorError: {
+      fill: theme.palette.error.main,
+    },
+    colorSuccess: {
+      fill: theme.palette.success.main,
+    },
+
+    sizeInherit: {
+      fontSize: "inherit",
+    },
+
+    sizeSmall: {
+      fontSize: `${theme.constants.generalUnit * 2}px`,
+    },
+    sizeMedium: {
+      fontSize: `${theme.constants.generalUnit * 3}px`,
+    },
+    sizeLarge: {
+      fontSize: `${theme.constants.generalUnit * 4}px`,
+    },
+  }),
+)
+
+export interface SvgIconProps {
+  children?: ReactNode | ReactNode[]
+  className?: string
+  color?: colorProp
+  fontSize?: fontSizeProp
+}
+
+const SvgIcon: React.FC<SvgIconProps> = ({
+  children,
+  className,
+  color = "inherit",
+  fontSize = "medium",
+  ...rest
+}: SvgIconProps) => {
+  const classes = useStyles()
+
+  const Component = "span"
+
+  return (
+    <Component
+      className={clsx(
+        classes.root,
+        {
+          [classes[`color${capitalize(color)}`]]: color !== "inherit",
+          [classes[`size${capitalize(fontSize)}`]]: true,
+        },
+        className,
+      )}
+      {...rest}
+    >
+      {children}
+    </Component>
+  )
+}
+
+export default SvgIcon
