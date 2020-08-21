7ba8b57198766b5bc3e9d8bd2c4dd6ecfa022492
tanmoy12 <tanmoy3399@gmail.com>
2020-08-21 16:22:34 +0600

    added emojies

---
+++ packages/common-components/src/Emoji/Emoji.tsx
@@ -0,0 +1,13 @@
+import Emoji from "a11y-react-emoji"
+import React from "react"
+
+export interface IEmojiProps {
+  symbol: string
+  label?: string
+}
+
+export const EmojiComponent: React.FC<IEmojiProps> = props => {
+  return <Emoji {...props} />
+}
+
+export default EmojiComponent
