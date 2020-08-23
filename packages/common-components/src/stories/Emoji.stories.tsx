5725040594d57d0a494f81bae661b585ba02c077
Tanmoy Basak Anjan <tanmoy3399@gmail.com>
2020-08-23 22:01:39 +0600

    Emoji (#44)
    * added emojies
    
    * emoji stories addedd
    
    * story update
    
    * added react router

---
+++ packages/common-components/src/stories/Emoji.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Emoji, SmileEmoji } from "../Emoji"
+
+export default {
+  title: "Emoji",
+  component: Emoji,
+  excludeStories: /.*Data$/,
+}
+
+export const SmileEmojiDemo = (): React.ReactNode => <SmileEmoji />
+
+export const CustomEmoji = (): React.ReactNode => <Emoji symbol="🚀" />
