fe184e5ffdcd22ba202394df03579340b44f6832
Michael Yankelev <myankelev@gmail.com>
2020-08-24 22:57:58 +0200

    add rollup CSS loader

--- packages/common-themes/rollup.config.js
+++ packages/common-themes/rollup.config.js
@@ -1,29 +1,31 @@
-import typescript from 'rollup-plugin-typescript2'
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import json from '@rollup/plugin-json'
-import nodePolyfills from 'rollup-plugin-node-polyfills'
-import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import typescript from "rollup-plugin-typescript2"
+import resolve from "@rollup/plugin-node-resolve"
+import commonjs from "@rollup/plugin-commonjs"
+import json from "@rollup/plugin-json"
+import nodePolyfills from "rollup-plugin-node-polyfills"
+import peerDepsExternal from "rollup-plugin-peer-deps-external"
+import postcss from "rollup-plugin-postcss"
 
 export default {
-  input: 'src/index.ts',
+  input: "src/index.ts",
   output: {
-    format: 'esm', // needs to be esm format as Onboard.js contains code-splitting
-    dir: 'dist/',
-    exports: 'named',
+    format: "esm", // needs to be esm format as Onboard.js contains code-splitting
+    dir: "dist/",
+    exports: "named",
     sourcemap: true,
-    strict: false
+    strict: false,
   },
   plugins: [
+    postcss(),
     peerDepsExternal(),
     typescript(),
     json(),
     resolve({
       browser: true,
-      preferBuiltins: true
+      preferBuiltins: true,
     }),
     commonjs(),
-    nodePolyfills()
+    nodePolyfills(),
   ],
-  external: ['react', 'react-dom', 'bnc-onboard']
+  external: ["react", "react-dom"],
 }
