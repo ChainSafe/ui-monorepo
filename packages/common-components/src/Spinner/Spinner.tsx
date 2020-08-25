fdf866f493071a94869eaacf23841dbdd0154b0a
RyRy79261 <ryanjnoble@gmail.com>
2020-08-25 17:21:12 +0200

    Working on adding package

--- packages/common-components/src/Spinner/Spinner.tsx
+++ packages/common-components/src/Spinner/Spinner.tsx
@@ -9,42 +9,6 @@ const useStyles = makeStyles((theme: ITheme) =>
     root: {
       margin: 0,
     },
-    h1: {
-      ...theme.typography.h1,
-    },
-    h2: {
-      ...theme.typography.h2,
-    },
-    h3: {
-      ...theme.typography.h3,
-    },
-    h4: {
-      ...theme.typography.h4,
-    },
-    h5: {
-      ...theme.typography.h5,
-    },
-    h6: {
-      ...theme.typography.h6,
-    },
-    subtitle1: {
-      ...theme.typography.subtitle1,
-    },
-    subtitle2: {
-      ...theme.typography.subtitle2,
-    },
-    body1: {
-      ...theme.typography.body1,
-    },
-    body2: {
-      ...theme.typography.body2,
-    },
-    caption: {
-      ...theme.typography.caption,
-    },
-    button: {
-      ...theme.typography.button,
-    },
   }),
 )
 
--- packages/common-components/src/Typography/index.ts
+++ packages/common-components/src/Typography/index.ts
@@ -1,1 +1,1 @@
-export { default } from "./Typography"
+export { default as Typography } from "./Typography"
