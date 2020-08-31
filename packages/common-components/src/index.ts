bc1c420ed618356cb418967c66cb447fdc8720fa
RyRy79261 <ryanjnoble@gmail.com>
2020-08-31 22:51:13 +0200

    Branch is broken

--- packages/common-components/src/index.ts
+++ packages/common-components/src/index.ts
@@ -1,20 +1,29 @@
 export * from "./Breadcrumb"
+export { default as Breadcrumb } from "./Breadcrumb"
 
 export * from "./Blockies"
+export { default as Blockies } from "./Blockies"
 
 export * from "./Button"
+export { default as Button } from "./Button"
 
 export * from "./Emoji"
+export { default as Emoji } from "./Emoji"
 
 export * from "./Icons"
+export { default as Icons } from "./Icons"
 
 export * from "./Modal"
+export { default as Modal } from "./Modal"
 
 export * from "./Router"
 export { ConditionalRoute } from "./Router"
 
 export * from "./Table"
+export { default as Table } from "./Table"
 
 export * from "./Typography"
+export { default as Typography } from "./Typography"
 
 export * from "./Spinner"
+export { default as Spinner } from "./Spinner"
