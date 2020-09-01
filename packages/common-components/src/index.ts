3291b136666df533c95bf3cc4b0cdae1719bc06f
RyRy79261 <ryanjnoble@gmail.com>
2020-09-01 22:38:56 +0200

    Cleaned exports

--- packages/common-components/src/index.ts
+++ packages/common-components/src/index.ts
@@ -1,23 +1,15 @@
-export * from "./Breadcrumb"
-export { default as Breadcrumb } from "./Breadcrumb"
-
 export * from "./Blockies"
-export { default as Blockies } from "./Blockies"
-
-export * from "./Button"
+export * from "./Breadcrumb"
 export * from "./Button"
 
 export * from "./Emoji"
-
 export * from "./Icons"
-export * from "./Button"
-export * from "./Emoji"
+
 export * from "./Modal"
-export * from "./ScrollbarWrapper"
 
 export * from "./Router"
 export { ConditionalRoute } from "./Router"
 
 export * from "./Table"
-
 export * from "./Typography"
+export * from "./ScrollbarWrapper"
