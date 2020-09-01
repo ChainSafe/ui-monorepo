44ea69511421447de9a5361e19a02405635a2ba0
tanmoy12 <tanmoy3399@gmail.com>
2020-09-01 21:22:31 +0600

    added icons

---
+++ packages/common-components/src/Icons/icons/InfoCircle.tsx
@@ -0,0 +1,5 @@
+import * as React from "react"
+import createSvgIcon from "../createSvgIcon"
+import { ReactComponent as InfoCircle } from "../svgs/infoCircle.svg"
+
+export default createSvgIcon(<InfoCircle />)
