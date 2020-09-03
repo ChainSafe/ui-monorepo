3ddcea0a234080d67fdfb9619a4d2955ca110031
Michael Yankelev <myankelev@gmail.com>
2020-09-04 00:25:43 +0200

    basic select input wired up

---
+++ packages/common-components/src/stories/SelectInput.stories.tsx
@@ -0,0 +1,54 @@
+import React from "react"
+import { withKnobs, select, boolean, text } from "@storybook/addon-knobs"
+import { action } from "@storybook/addon-actions"
+import { Formik, Form } from "formik"
+import { Button } from "../Button"
+import { FormikSelectInput, SelectInput } from "../SelectInput"
+import SelectOption from "../SelectInput/SelectOption"
+
+export default {
+  title: "Select Input",
+  component: SelectInput,
+  excludeStories: /.*Data$/,
+  decorators: [withKnobs],
+}
+
+const sizeOptions: ["large", "medium", "small"] = ["large", "medium", "small"]
+
+export const actionsData = {
+  onChange: action("onChange"),
+  onFormSubmit: action("Submit Form"),
+}
+
+export const SelectInputStory = (): React.ReactNode => (
+  <SelectInput
+    onChange={() => actionsData.onChange()}
+    size={select("Size", sizeOptions, "large")}
+    disabled={boolean("Disabled", false)}
+    label={text("Label", "Testing Label")}
+  >
+    <SelectOption label="a" />
+    <SelectOption label="b" />
+  </SelectInput>
+)
+
+export const FormikStory = (): React.ReactNode => {
+  return (
+    <Formik
+      initialValues={{
+        select: undefined,
+      }}
+      onSubmit={(values: any) => actionsData.onFormSubmit(values)}
+    >
+      <Form>
+        <FormikSelectInput name="select">
+          <SelectOption label="a" />
+          <SelectOption label="b" />
+        </FormikSelectInput>
+        <br />
+        <br />
+        <Button type="submit">Submit</Button>
+      </Form>
+    </Formik>
+  )
+}
