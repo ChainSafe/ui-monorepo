1550b796df04b776b4fdb38402421f0a562a8416
Michael Yankelev <myankelev@gmail.com>
2020-09-08 17:23:50 +0200

    fix textinput type naming

--- packages/common-components/src/TextInput/TextInput.tsx
+++ packages/common-components/src/TextInput/TextInput.tsx
@@ -296,7 +296,7 @@ const useStyles = makeStyles((theme: ITheme) =>
   }),
 )
 
-export type inputState = "normal" | "warning" | "success" | "error"
+export type InputState = "normal" | "warning" | "success" | "error"
 
 export interface TextInputProps {
   className?: string
@@ -307,7 +307,7 @@ export interface TextInputProps {
   disabled?: boolean
   LeftIcon?: typeof SvgIcon
   RightIcon?: typeof SvgIcon
-  state?: inputState
+  state?: InputState
   size?: "large" | "medium" | "small"
   captionMessage?: string
   onChange: (e: ChangeEvent<HTMLInputElement>) => void
