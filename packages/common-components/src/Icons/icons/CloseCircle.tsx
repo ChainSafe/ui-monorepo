96429a2f6b5d99a58170382c7b26c61d5c5ab7fa
Tanmoy Basak Anjan <tanmoy3399@gmail.com>
2020-09-11 17:46:06 +0600

    Toaster (#84)
    * initial
    
    * added icons
    
    * added toaster
    
    * added styles
    
    * stories ready
    
    * removed transition groups
    
    * added exports
    
    * adding shadows
    
    * added shadows
    
    * toaster ready
    
    * toaster ready
    
    * animation fix

---
+++ packages/common-components/src/Icons/icons/CloseCircle.tsx
@@ -0,0 +1,5 @@
+import * as React from "react"
+import createSvgIcon from "../createSvgIcon"
+import { ReactComponent as CloseCircle } from "../svgs/closeCircle.svg"
+
+export default createSvgIcon(<CloseCircle />)
