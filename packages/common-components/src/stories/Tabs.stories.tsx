ad8048aba656e52eec848700ecd34f5fb3a4f1a5
Tanmoy Basak Anjan <tanmoy3399@gmail.com>
2020-09-23 17:05:01 +0600

    Tabs (#160)
    * complete
    
    * console removed

---
+++ packages/common-components/src/stories/Tabs.stories.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+import { Tabs, TabPane } from "../Tabs"
+
+export default {
+  title: "Tabs",
+  component: Tabs,
+}
+
+export const TabsDemo = (): React.ReactNode => {
+  const [tab, setTab] = React.useState("1")
+  return (
+    <Tabs activeKey={tab} onTabSelect={setTab}>
+      <TabPane title="tab 1" tabKey="1">
+        first
+      </TabPane>
+      <TabPane title="tab 2" tabKey="2">
+        second
+      </TabPane>
+      <TabPane title="tab 3" tabKey="3">
+        third
+      </TabPane>
+    </Tabs>
+  )
+}
