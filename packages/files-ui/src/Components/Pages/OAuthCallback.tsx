797b65e90eddf4c534df39a8f1c11d4146b1303e
tanmoy12 <tanmoy3399@gmail.com>
2020-10-09 01:43:49 +0600

    oauths working

--- packages/files-ui/src/Components/Pages/OAuthCallback.tsx
+++ packages/files-ui/src/Components/Pages/OAuthCallback.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react"
-import { useLocation, useParams } from "@chainsafe/common-components"
-import { useImployApi } from "@chainsafe/common-contexts"
+import { useLocation, useParams } from "@imploy/common-components"
+import { useImployApi } from "@imploy/common-contexts"
 
 function useQueryParams() {
   return new URLSearchParams(useLocation().search)
