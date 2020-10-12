986bcfb0fad15443bc67c14b16956789a1df476f
RyRy79261 <ryanjnoble@gmail.com>
2020-10-12 16:36:05 +0200

    Header & search updated

---
+++ packages/common-components/src/HamburgerMenu/HamburgerMenu.tsx
@@ -0,0 +1,117 @@
+import { createStyles, ITheme, makeStyles } from "@imploy/common-themes"
+import React from "react"
+import clsx from "clsx"
+
+const useStyles = makeStyles(
+  ({ animation, breakpoints, constants, palette }: ITheme) => {
+    const strokeHeight = 2
+    const defaultSize = 24
+    return createStyles({
+      root: {
+        transitionDuration: `${animation.transform}ms`,
+        padding: constants.generalUnit * 1.25,
+        cursor: "pointer",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        "& span": {
+          position: "absolute",
+          transitionDuration: `${animation.transform}ms`,
+          borderRadius: strokeHeight,
+          backgroundColor: palette.common.black.main,
+        },
+        "&.default": {
+          "& span": {
+            width: "100%",
+            height: strokeHeight,
+            transform: "translateY(-50%)",
+            left: 0,
+            "&:first-child": {
+              top: `calc(50% - ${constants.generalUnit}px)`,
+            },
+            "&:nth-child(2)": {
+              top: "50%",
+            },
+            "&:last-child": {
+              top: `calc(50% + ${constants.generalUnit}px)`,
+              width: "80%",
+            },
+          },
+          "&:hover": {
+            [breakpoints.up("sm")]: {
+              "& span": {
+                "&:last-child": {
+                  width: "100%",
+                },
+              },
+            },
+          },
+        },
+        "&.active": {
+          "& span": {
+            width: "100%",
+            height: strokeHeight,
+            transform: "translateY(-50%)",
+            left: 0,
+            "&:first-child": {
+              top: `calc(50% - ${constants.generalUnit}px)`,
+            },
+            "&:nth-child(2)": {
+              top: "50%",
+            },
+            "&:last-child": {
+              top: `calc(50% + ${constants.generalUnit}px)`,
+            },
+          },
+          "&:hover": {
+            [breakpoints.up("sm")]: {
+              "& span": {
+                "&:last-child": {
+                  width: "80%",
+                },
+              },
+            },
+          },
+        },
+
+        // TODO: Add loading option
+        // "&.loading": {
+        //   "&:hover": {
+        //      [breakpoints.up("sm")]: {
+
+        //     }
+        //   }
+        // },
+      },
+      inner: {
+        transitionDuration: `${animation.transform}ms`,
+        position: "relative",
+        width: defaultSize,
+        height: defaultSize,
+      },
+    })
+  },
+)
+
+interface IHamburgerMenu extends React.HTMLProps<HTMLDivElement> {
+  variant: "default" | "active"
+  // | "loading"
+}
+
+const HamburgerMenu: React.FC<IHamburgerMenu> = ({
+  variant = "default",
+}: IHamburgerMenu) => {
+  const classes = useStyles()
+  return (
+    <section className={clsx(classes.root, variant)}>
+      <div className={classes.inner}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </section>
+  )
+}
+
+export default HamburgerMenu
--- packages/files-ui/src/Components/Layouts/AppWrapper.tsx
+++ packages/files-ui/src/Components/Layouts/AppWrapper.tsx
@@ -6,7 +6,7 @@ import {
   useMediaQuery,
   useTheme,
 } from "@imploy/common-themes"
-import React, { Fragment, useCallback } from "react"
+import React, { Fragment, useCallback, useState } from "react"
 import { ReactNode } from "react"
 import clsx from "clsx"
 import {
@@ -14,6 +14,7 @@ import {
   Typography,
   ChainsafeFilesLogo,
   DatabaseSvg,
+  HamburgerMenu,
   SettingSvg,
   MenuDropdown,
   PowerDownSvg,
@@ -35,6 +36,8 @@ const useStyles = makeStyles(
     const topPadding = constants.generalUnit * 3
     const accountControlsPadding = constants.generalUnit * 7
 
+    const mobileHeaderHeight = constants.generalUnit * 6.3
+
     return createStyles({
       root: {
         minHeight: "100vh",
@@ -44,30 +47,51 @@ const useStyles = makeStyles(
         display: "flex",
         flexDirection: "row",
         alignItems: "center",
-        "& img": {
-          height: constants.generalUnit * 5,
-          width: "auto",
+
+        [breakpoints.up("sm")]: {
+          "& img": {
+            height: constants.generalUnit * 5,
+            width: "auto",
+          },
+          "& > *:first-child": {
+            marginRight: constants.generalUnit,
+          },
         },
-        "& > *:first-child": {
-          marginRight: constants.generalUnit,
+        [breakpoints.down("sm")]: {
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          transform: "translate(-50%,-50%)",
+          "& img": {
+            height: constants.generalUnit * 3.25,
+            width: "auto",
+          },
         },
       },
       nav: {
         width: 0,
-        backgroundColor: palette.additional["gray"][3],
-        height: "100%",
         overflow: "hidden",
         transitionDuration: `${animation.translate}ms`,
         display: "flex",
         flexDirection: "column",
         position: "fixed",
-        top: 0,
         left: 0,
         opacity: 0,
-        padding: `${topPadding}px ${constants.generalUnit * 4.5}px`,
         "&.active": {
           opacity: 1,
-          width: modalWidth,
+        },
+        [breakpoints.up("sm")]: {
+          padding: `${topPadding}px ${constants.generalUnit * 4.5}px`,
+          backgroundColor: palette.additional["gray"][3],
+          top: 0,
+          height: "100%",
+          "&.active": {
+            width: modalWidth,
+          },
+        },
+        [breakpoints.down("sm")]: {
+          height: `calc(100% - ${mobileHeaderHeight}px)`,
+          top: mobileHeaderHeight,
         },
       },
       navMenu: {
@@ -128,14 +152,17 @@ const useStyles = makeStyles(
         display: "flex",
         flexDirection: "row",
         top: 0,
-        width: `calc(100% - ${modalWidth}px)`,
-        padding: `${0}px ${contentPadding}px ${0}px ${contentPadding}px`,
         transitionDuration: `${animation.translate}ms`,
         visibility: "hidden",
         [breakpoints.up("sm")]: {
+          width: `calc(100% - ${modalWidth}px)`,
+          padding: `${0}px ${contentPadding}px ${0}px ${contentPadding}px`,
           left: modalWidth,
           backgroundColor: palette.common.white.main,
           opacity: 0,
+          "& > *:first-child": {
+            flex: "1 1 0",
+          },
           "&.active": {
             opacity: 1,
             height: "auto",
@@ -145,11 +172,16 @@ const useStyles = makeStyles(
         },
         [breakpoints.down("sm")]: {
           left: 0,
-          backgroundColor: palette.common.white.main,
-          "&.active": {},
-        },
-        "& >*:first-child": {
-          flex: "1 1 0",
+          width: "100%",
+          justifyContent: "space-between",
+          alignItems: "center",
+          position: "fixed",
+          backgroundColor: palette.additional["gray"][3],
+          "&.active": {
+            opacity: 1,
+            visibility: "visible",
+            height: mobileHeaderHeight,
+          },
         },
       },
       accountControls: {
@@ -188,6 +220,18 @@ const useStyles = makeStyles(
         },
         [breakpoints.down("sm")]: {
           minHeight: "100vh",
+          "&.active": {
+            height: "initial",
+            padding: `${mobileHeaderHeight}px 0 0`,
+          },
+        },
+      },
+      searchModule: {
+        [breakpoints.down("sm")]: {
+          position: "fixed",
+          top: 0,
+          right: 0,
+          height: mobileHeaderHeight,
         },
       },
     })
@@ -195,11 +239,14 @@ const useStyles = makeStyles(
 )
 
 const AppWrapper: React.FC<IAppWrapper> = ({ children }: IAppWrapper) => {
-  const { isLoggedIn, logout } = useImployApi()
-  const { getProfileTitle, removeUser } = useUser()
   const classes = useStyles()
   const { breakpoints }: ITheme = useTheme()
   const desktop = useMediaQuery(breakpoints.up("sm"))
+
+  const [navOpen, setNavOpen] = useState<boolean>(desktop)
+
+  const { isLoggedIn, logout } = useImployApi()
+  const { getProfileTitle, removeUser } = useUser()
 
   const signOut = useCallback(() => {
     logout()
@@ -211,17 +258,19 @@ const AppWrapper: React.FC<IAppWrapper> = ({ children }: IAppWrapper) => {
       <CssBaseline />
       <section
         className={clsx(classes.nav, {
-          active: isLoggedIn,
+          active: desktop ? isLoggedIn : navOpen,
         })}
       >
         {isLoggedIn && (
           <Fragment>
-            <div>
-              <Link className={classes.logo} to={ROUTE_LINKS.Home}>
-                <ChainsafeFilesLogo />
-                <Typography variant="h5">Files</Typography>
-              </Link>
-            </div>
+            {desktop && (
+              <div>
+                <Link className={classes.logo} to={ROUTE_LINKS.Home}>
+                  <ChainsafeFilesLogo />
+                  <Typography variant="h5">Files</Typography>
+                </Link>
+              </div>
+            )}
             <div className={classes.linksArea}>
               <Typography>Folders</Typography>
               <nav className={classes.navMenu}>
@@ -282,25 +331,37 @@ const AppWrapper: React.FC<IAppWrapper> = ({ children }: IAppWrapper) => {
         >
           {isLoggedIn && (
             <Fragment>
-              <SearchModule />
-              {desktop && (
-                <section className={classes.accountControls}>
-                  <MenuDropdown
-                    title={getProfileTitle()}
-                    anchor="bottom-right"
-                    menuItems={[
-                      {
-                        onClick: () => signOut(),
-                        contents: (
-                          <div className={classes.menuItem}>
-                            <PowerDownSvg />
-                            <Typography>Sign Out</Typography>
-                          </div>
-                        ),
-                      },
-                    ]}
+              {desktop ? (
+                <Fragment>
+                  <section className={classes.accountControls}>
+                    <MenuDropdown
+                      title={getProfileTitle()}
+                      anchor="bottom-right"
+                      menuItems={[
+                        {
+                          onClick: () => signOut(),
+                          contents: (
+                            <div className={classes.menuItem}>
+                              <PowerDownSvg />
+                              <Typography>Sign Out</Typography>
+                            </div>
+                          ),
+                        },
+                      ]}
+                    />
+                  </section>
+                </Fragment>
+              ) : (
+                <Fragment>
+                  <HamburgerMenu
+                    onClick={() => setNavOpen(!navOpen)}
+                    variant={navOpen ? "active" : "default"}
                   />
-                </section>
+                  <Link className={classes.logo} to={ROUTE_LINKS.Home}>
+                    <ChainsafeFilesLogo />
+                  </Link>
+                  <SearchModule className={classes.searchModule} />
+                </Fragment>
               )}
             </Fragment>
           )}
