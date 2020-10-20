260f0df94a97bc96c7adf0be41896d0a940815c1
Michael Yankelev <12774278+FSM1@users.noreply.github.com>
2020-10-20 17:42:46 +0200

    File Preview Modal (#275)
    * initial commit
    
    * fix styling
    
    * fix next/prev styles and not supported

---
+++ packages/common-components/src/Icons/icons/ArrowRight.icon.tsx
@@ -0,0 +1,7 @@
+import * as React from "react"
+import createSvgIcon from "../createSvgIcon"
+import { ReactComponent as ArrowRightSvg } from "../svgs/arrow-right.svg"
+
+export { ArrowRightSvg }
+
+export default createSvgIcon(<ArrowRightSvg />)
