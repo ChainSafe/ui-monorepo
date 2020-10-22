44d44c8e322f24f7d197a4a95bb074667dab9a05
RyRy79261 <ryanjnoble@gmail.com>
2020-10-22 17:00:48 +0200

    Added icon

---
+++ packages/common-components/src/Icons/icons/QuestionCircle.icon.tsx
@@ -0,0 +1,7 @@
+import * as React from "react"
+import createSvgIcon from "../createSvgIcon"
+import { ReactComponent as QuestionCircleSvg } from "../svgs/more.svg"
+
+export { QuestionCircleSvg }
+
+export default createSvgIcon(<QuestionCircleSvg />)
