3d9b31ae6787f4f44db01ab4ae95308e364ee956
RyRy79261 <ryanjnoble@gmail.com>
2020-10-23 13:13:45 +0200

    Added inverse exclaim

---
+++ packages/common-components/src/Icons/icons/ExclamationCircleInverse.icon.tsx
@@ -0,0 +1,7 @@
+import * as React from "react"
+import createSvgIcon from "../createSvgIcon"
+import { ReactComponent as ExclamationCircleInverseSvg } from "../svgs/exclaimation-circle-inverse.svg"
+
+export { ExclamationCircleInverseSvg }
+
+export default createSvgIcon(<ExclamationCircleInverseSvg />)
