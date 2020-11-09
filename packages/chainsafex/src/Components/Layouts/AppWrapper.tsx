1b5b7fa662bef771681da23ec7187a34c79eddbc
RyRy79261 <ryanjnoble@gmail.com>
2020-11-09 16:32:22 +0200

    Added section to repo

---
+++ packages/chainsafex/src/Components/Layouts/AppWrapper.tsx
@@ -0,0 +1,21 @@
+import { createStyles, ITheme, makeStyles } from "@imploy/common-themes"
+import React from "react"
+import { ReactNode } from "react"
+
+interface IAppWrapper {
+  children: ReactNode | ReactNode[]
+}
+
+const useStyles = makeStyles(({}: ITheme) => {
+  return createStyles({
+    root: {},
+  })
+})
+
+const AppWrapper: React.FC<IAppWrapper> = ({ children }: IAppWrapper) => {
+  const classes = useStyles()
+
+  return <div className={classes.root}>{children}</div>
+}
+
+export default AppWrapper
---
+++ packages/chainsafex/src/Components/Routes.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { Switch, ConditionalRoute } from "@imploy/common-components"
+import HomePage from "./Pages/HomePage"
+
+export const ROUTE_LINKS = {
+  Landing: "/",
+}
+
+const Routes = () => {
+  return (
+    <Switch>
+      <ConditionalRoute
+        exact
+        path={ROUTE_LINKS.Landing}
+        isAuthorized={true}
+        component={HomePage}
+        redirectPath={ROUTE_LINKS.Landing}
+      />
+    </Switch>
+  )
+}
+
+export default Routes
