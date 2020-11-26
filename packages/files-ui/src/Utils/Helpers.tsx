f9987975730bea51558732a9fb878532decb7818
RyRy79261 <ryanjnoble@gmail.com>
2020-11-26 10:50:00 +0200

    Search bard fixed

--- packages/files-ui/src/Utils/Helpers.tsx
+++ packages/files-ui/src/Utils/Helpers.tsx
@@ -1,4 +1,7 @@
 export const shortenAddress = (address: string, remaining: number = 6) => {
+  if (address.length <= remaining * 2) {
+    return address
+  }
   return `${address.substr(0, remaining)}...${address.substr(
     address.length - remaining,
     remaining,
