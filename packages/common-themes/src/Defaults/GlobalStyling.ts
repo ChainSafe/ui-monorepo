e9d966559fce82e103be9607ec007a49b53204ae
tanmoy12 <tanmoy3399@gmail.com>
2020-12-14 22:53:02 +0600

    theme level typography fix

--- packages/common-themes/src/Defaults/GlobalStyling.ts
+++ packages/common-themes/src/Defaults/GlobalStyling.ts
@@ -1,4 +1,5 @@
-import { DefaultThemeConfig } from "./ThemeConfig"
+import { DefaultThemeConfig, defaultFontFamilyStack } from "./ThemeConfig"
+
 export const DefaultGlobalStyling = {
   html: {
     ...DefaultThemeConfig.typography.global,
@@ -7,17 +8,17 @@ export const DefaultGlobalStyling = {
     // Change from `box-sizing: content-box` so that `width`
     // is not affected by `padding` or `border`.
     boxSizing: "border-box",
- 
   },
-  '*, *:before, *:after': {
-    boxSizing: 'inherit',
+  "*, *:before, *:after": {
+    boxSizing: "inherit",
   },
-  'strong, b': {
+  "strong, b": {
     fontWeight: DefaultThemeConfig.typography.fontWeight.bold,
   },
   body: {
     color: DefaultThemeConfig.palette.text.primary,
     ...DefaultThemeConfig.typography.body2,
+    ...defaultFontFamilyStack,
     backgroundColor: DefaultThemeConfig.palette.background.default,
     "& @media print": {
       // Save printer ink.
