682cccf18fb2dfcb67c05e1ee9805b8b701ec12a
tanmoy12 <tanmoy3399@gmail.com>
2021-01-04 23:36:52 +0600

    updated UI

--- packages/files-ui/src/Components/Layouts/AppHeader.tsx
+++ packages/files-ui/src/Components/Layouts/AppHeader.tsx
@@ -207,6 +207,8 @@ const AppHeader: React.FC<IAppHeader> = ({
                   />
                   <Link className={classes.logo} to={ROUTE_LINKS.Home}>
                     <ChainsafeFilesLogo />
+                    &nbsp;
+                    <Typography variant="caption">beta</Typography>
                   </Link>
                 </>
               )}
@@ -215,12 +217,6 @@ const AppHeader: React.FC<IAppHeader> = ({
                 searchActive={searchActive}
                 setSearchActive={setSearchActive}
               />
-              <Link className={classes.logo} to={ROUTE_LINKS.Home}>
-                <ChainsafeFilesLogo />
-                &nbsp;
-                <Typography variant="caption">beta</Typography>
-              </Link>
-              {/* <SearchModule className={classes.searchModule} /> */}
             </Fragment>
           )}
         </Fragment>
