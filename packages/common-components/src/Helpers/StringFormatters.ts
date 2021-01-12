2be39dd9db0b2719436bff6a333f93a88393ee2d
Michael Yankelev <myankelev@gmail.com>
2021-01-12 13:25:21 +0200

    add correct suffixes for base 2

--- packages/common-components/src/Helpers/StringFormatters.ts
+++ packages/common-components/src/Helpers/StringFormatters.ts
@@ -6,10 +6,14 @@ export const formatBytes = (
   if (0 === sizeInBytes) return "0 Bytes"
   const c = 0 > decimals ? 0 : decimals
   const units = base === 2 ? 1024 : 1000
+  const suffixes =
+    base === 2
+      ? ["Bytes", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"]
+      : ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
   const d = Math.floor(Math.log(sizeInBytes) / Math.log(units))
   return (
     parseFloat((sizeInBytes / Math.pow(units, d)).toFixed(c)) +
     " " +
-    ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"][d]
+    suffixes[d]
   )
 }
