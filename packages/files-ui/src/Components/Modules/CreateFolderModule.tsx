8cdfd4000a39ac9d5c1d9f0e5263d842fa62f0b5
tanmoy12 <tanmoy3399@gmail.com>
2021-01-15 23:27:57 +0600

    timeout change

--- packages/files-ui/src/Components/Modules/CreateFolderModule.tsx
+++ packages/files-ui/src/Components/Modules/CreateFolderModule.tsx
@@ -86,7 +86,7 @@ const CreateFolderModule: React.FC<ICreateFolderModuleProps> = ({
 
   useEffect(() => {
     if (modalOpen) {
-      setTimeout(() => inputRef.current.focus(), 50)
+      setTimeout(() => inputRef.current.focus(), 100)
     }
   }, [modalOpen])
 
