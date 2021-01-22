d630fa048f04dda2294630cbb8ac6f79e5f31d0e
RyRy79261 <ryanjnoble@gmail.com>
2021-01-22 14:43:59 +0200

    Decoupled markup

--- packages/files-ui/src/Components/Modules/FileBrowserModule/types.ts
+++ packages/files-ui/src/Components/Modules/FileBrowserModule/types.ts
@@ -1,9 +1,30 @@
+import { Crumb } from "@chainsafe/common-components"
+import { IFile } from "../../../Contexts/DriveContext"
+
 export type FileOperation = "rename" | "delete" | "download" | "share" | "move"
 
-export interface IFileBrowserProps {
+export interface IFilesTableBrowserProps {
   heading?: string
   // TODO: once pagination & unique content requests are present, this might change to a passed in function
   controls?: boolean
   fileOperations: FileOperation[]
   folderOperations: FileOperation[]
+
+  handleRename: (path: string, new_path: string) => Promise<void>
+  handleMove: (path: string, new_path: string) => Promise<void>
+  downloadFile: (cid: string) => Promise<void>
+  deleteFile: (cid: string) => Promise<void>
+
+  handleUploadOnDrop: (
+    files: File[],
+    fileItems: DataTransferItemList,
+    path: string,
+  ) => void
+  updateCurrentPath: (newPath: string) => void
+
+  sourceFiles: IFile[]
+  currentPath: string
+  crumbs: Crumb[]
 }
+
+export interface IFilesBrowserModuleProps extends IFilesTableBrowserProps {}
