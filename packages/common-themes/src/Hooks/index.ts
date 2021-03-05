0a5cc61fab709ab7de838a371fe8a5863ae4f3df
Thibaut Sardan <hi@thib.top>
2021-03-05 13:14:00 +0100

    fix loop

--- packages/common-themes/src/Hooks/index.ts
+++ packages/common-themes/src/Hooks/index.ts
@@ -1,5 +1,4 @@
 import { useOnClickOutside } from "./useOnClickOutside"
 import { useMediaQuery } from "./useMediaQuery"
-import { useDoubleClick } from "./useDoubleClick"
 
-export { useOnClickOutside, useMediaQuery, useDoubleClick }
+export { useOnClickOutside, useMediaQuery }
---
+++ packages/files-ui/src/Hooks/index.ts
@@ -0,0 +1,1 @@
+export {useDoubleClick} from './useDoubleClick'
