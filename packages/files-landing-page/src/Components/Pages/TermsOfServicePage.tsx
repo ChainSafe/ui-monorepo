57b89e485a1ed5c312419963109b5801d90993c7
sweetpea22 <cindychau24@me.com>
2021-03-22 10:23:55 -0400

    Update terms

--- packages/files-landing-page/src/Components/Pages/TermsOfServicePage.tsx
+++ packages/files-landing-page/src/Components/Pages/TermsOfServicePage.tsx
@@ -77,20 +77,14 @@ const TermsOfServicePage: React.FC = () => {
           component="p"
           className={clsx(classes.caption, classes.padSmall)}
         >
-          <Trans>Last Modified: November 13, 2020</Trans>
+          <Trans>Last Modified: March 15 2021</Trans>
         </Typography>
         <Typography
           component="p"
           className={clsx(classes.text, classes.padSmall)}
         >
           <Trans>
-            Hello! The following are the terms and conditions for use of
-            ChainSafe Files (“Files”), each feature individually and
-            collectively referred to as the “Service”. Please read them
-            carefully. This Service is provided to individuals who are over the
-            age of 13. As used in these terms of service, “ChainSafe”, “We”, and
-            the “Company” mean ChainSafe Systems, while “you” refers to the user
-            or Account holder.{" "}
+            Hello! Here are the terms and conditions for use of ChainSafe Files (“Files”), its features collectively referred to as the “Service”. This Service is provided to persons who are over the age of 13. As used in these terms of service, “ChainSafe”, “We”, and the “Company” mean ChainSafe Systems, while “you” refers to you, the user or Account holder..{" "}
           </Trans>
         </Typography>
         <Typography
@@ -99,35 +93,19 @@ const TermsOfServicePage: React.FC = () => {
           className={clsx(classes.text, classes.padSmall)}
         >
           <Trans>
-            Each user is responsible for his or her transmissions through their
-            Files Account. As a condition of your use of the Service, you affirm
-            that you will not use the Service for any purpose that is unlawful
-            or prohibited by these terms, conditions, and notices. By
-            registering with ChainSafe Files, you agree to be bound by the terms
-            outlined here. ChainSafe may, at its sole discretion, immediately
-            terminate service without cause or notice, should users’ conduct
-            fail to conform to these conditions:
+            Each user is responsible for their transmissions through their Files Account. As a condition of your use of the Service, you affirm that you will not use the Service for any unlawful or prohibited purpose. By registering with ChainSafe Files, you agree to be bound by the terms outlined here. If your conduct fails to conform to these conditions, ChainSafe may, at its sole discretion, immediately terminate service without cause or notice.
           </Trans>
         </Typography>
 
         <ol className={clsx(classes.orderedList, classes.padLarge)}>
           <li>
-            This is an internet-based service. You agree to be bound by Internet
-            regulations, policies and procedures.
+           This is an internet-based service. You agree to be bound by Internet regulations, policies and procedures.
           </li>
           <li>
-            You agree to comply with all applicable local, state, national and
-            international laws and regulations. You also agree to not use your
-            Files Account for illegal purposes; to not interfere or disrupt
-            networks connected to this service; and to comply with all the
-            rules, policies and procedures connected to this service.
+            You agree to comply with all applicable local, state, national and international laws and regulations. You also agree to not use your Files Account for illegal purposes; to not interfere or disrupt networks connected to this service; and to comply with all the rules, policies and procedures connected to this service.
           </li>
           <li>
-            You agree not to store any objectionable material including, but not
-            limited to, unlawful or harassing, libelous, abusive, threatening,
-            harmful, vulgar or obscene material that constitutes or encourages
-            conduct that could constitute a criminal offence, give rise to civil
-            liability or otherwise violate items discussed in #2 above.{" "}
+            You agree not to store any objectionable material including, but not limited to, unlawful or harassing, libelous, abusive, threatening, harmful, vulgar or obscene material that constitutes or encourages conduct that could constitute a criminal offence, give rise to civil liability or otherwise violate items discussed in #2 above.{" "}
           </li>
         </ol>
 
@@ -144,13 +122,7 @@ const TermsOfServicePage: React.FC = () => {
           className={clsx(classes.text, classes.padSmall)}
         >
           <Trans>
-            ChainSafe has the right to discontinue service for fraudulent
-            payments. Unless you cancel your subscription before the end of the
-            applicable subscription period, your subscription will automatically
-            renew, and you authorize Stripe to collect the monthly subscription
-            fee. Subscriptions can be cancelled at any time in your account
-            settings. You are responsible for keeping your payment information
-            up to date.
+            Unless you cancel your subscription before the end of the applicable subscription period, your subscription will automatically renew, and you authorize Stripe to collect the monthly subscription fee. Subscriptions can be cancelled at any time in your account settings. You are responsible for keeping your payment information up to date. ChainSafe has the right to discontinue service for fraudulent payments. 
           </Trans>
         </Typography>
         <Typography
@@ -159,12 +131,7 @@ const TermsOfServicePage: React.FC = () => {
           className={clsx(classes.text, classes.padSmall)}
         >
           <Trans>
-            For payments made with cryptocurrency, we are not responsible for
-            payments made to the wrong crypto address. We cannot reverse any
-            transaction which has been broadcasted to the provided blockchain
-            network. Should you want to purchase a plan with cryptocurrency, you
-            will be required to pay the annual, rather than monthly,
-            subscription fee.
+            For payments made with cryptocurrency, we are not responsible for payments made to the wrong crypto address. We cannot reverse any transaction which has been broadcasted to the provided blockchain network. Should you want to purchase a plan with cryptocurrency, you will be required to pay the annual, rather than monthly, subscription fee.
           </Trans>
         </Typography>
 
@@ -181,12 +148,7 @@ const TermsOfServicePage: React.FC = () => {
           className={clsx(classes.text, classes.padSmall)}
         >
           <Trans>
-            If there is any indication that you are using your account for
-            illegal activity, your account will be terminated immediately and
-            without notice. Activities that are absolutely not tolerated include
-            the storage, possession or distribution of child pornography,
-            terrorist activity, and storage of any material that is illegal,
-            fraudulent or threatening in relevant jurisdictions.
+           If there is any indication that you are using your account for illegal activity, your account will be terminated immediately without notice. Activities that are absolutely not tolerated include the storage, possession or distribution of child pornography, terrorist activity, and of any material that is illegal, fraudulent or threatening in relevant jurisdictions.
           </Trans>
         </Typography>
 
@@ -203,14 +165,7 @@ const TermsOfServicePage: React.FC = () => {
           className={clsx(classes.text, classes.padSmall)}
         >
           <Trans>
-            ChainSafe may make improvements and/or changes to the Service at any
-            time without notice. We do not guarantee that Files will be
-            uninterrupted or error-free, that bugs or malfunctions will be
-            corrected, or that the Service and its servers are free of harmful
-            components. We make no warranty or covenant on the availability,
-            reliability, or timeliness of the Service, and we are not
-            responsible for the appropriateness of the Service for any
-            particular purpose.
+            ChainSafe may make improvements and/or changes to the Service at any time without notice. We do not guarantee that Files will be uninterrupted or error-free, that bugs or malfunctions will be corrected, or that the Service and its servers are free of harmful components. We make no warranty or covenant on the availability, reliability, or timeliness of the Service, and we are not responsible for the appropriateness of the Service for any particular purpose.
           </Trans>
         </Typography>
         <Typography
@@ -219,13 +174,7 @@ const TermsOfServicePage: React.FC = () => {
           className={clsx(classes.text, classes.padSmall)}
         >
           <Trans>
-            ChainSafe will not be held responsible for any and all damages
-            whatsoever as a result of the loss of use, data or profits connected
-            to the performance of your Files Account. The user is solely
-            responsible for any and all information that passes and is stored
-            through it. We don’t take responsibility for the implications of any
-            attempt to use this website for activities or virtual currencies
-            that Files does not support.
+            ChainSafe will not be held responsible for any and all damages whatsoever as a result of the loss of use, data or profits connected to the performance of your Files Account. The user is solely responsible for any and all information that passes and is stored through it. We don’t take responsibility for the implications of any attempt to use this website for activities or virtual currencies that Files does not support.
           </Trans>
         </Typography>
         <Typography
@@ -234,17 +183,7 @@ const TermsOfServicePage: React.FC = () => {
           className={clsx(classes.text, classes.padSmall)}
         >
           <Trans>
-            The Company does not own or control the Filecoin Network or the
-            underlying software protocols which govern the operation of our
-            website. In general, the underlying protocols are licensed as open
-            source, and anyone can use, copy, modify, and distribute them. By
-            using the Website, you acknowledge and agree (i) that Files is not
-            responsible for operation of the Filecoin Network and that Files
-            makes no guarantee of their functionality, security, or
-            availability; and (ii) that the Filecoin Network is subject to
-            sudden changes in operating rules (“forks”), and that such forks may
-            materially affect the value, function, and/or even the name of
-            Filecoin you store in the Filecoin Network.
+           The Company does not own or control the Filecoin Network or the underlying software protocols which govern the operation of our website. In general, the underlying protocols are licensed as open source, and anyone can use, copy, modify, and distribute them. By using the Website, you acknowledge and agree (i) that Files is not responsible for operation of the Filecoin Network and that Files makes no guarantee of their functionality, security, or availability; and (ii) that the Filecoin Network is subject to sudden changes in operating rules (“forks”), and that such forks may materially affect the value, function, and/or even the name of Filecoin you store in the Filecoin Network.
           </Trans>
         </Typography>
 
@@ -261,9 +200,7 @@ const TermsOfServicePage: React.FC = () => {
           className={clsx(classes.text, classes.padSmall)}
         >
           <Trans>
-            You agree that ChainSafe, its officers and employees, cannot be held
-            responsible for any third-party claim, demand, or damage arising out
-            of your use of the Service.
+            You agree that the Company, its officers and employees, cannot be held responsible for any third-party claim, demand, or damage arising out of your use of the Service.
           </Trans>
         </Typography>
 
@@ -280,14 +217,7 @@ const TermsOfServicePage: React.FC = () => {
           className={clsx(classes.text, classes.padSmall)}
         >
           <Trans>
-            You agree that ChainSafe is not responsible for any deletion or
-            failure to store media or other information. ChainSafe Files can be
-            used for free until 20GB of storage is reached. At this point,
-            storage beyond the free amount must be paid for via a monthly or
-            yearly subscription. 
-            
-            {/* There are three payment tiers depending on the
-            needs of the user. */}
+           You agree that the Company is not responsible for any deletion or failure to store media or other information. ChainSafe Files can be used for free until 20GB of storage is reached. At this point, storage beyond this amount must be paid for via a monthly or yearly subscription fee.
           </Trans>
         </Typography>
 
@@ -304,10 +234,7 @@ const TermsOfServicePage: React.FC = () => {
           className={clsx(classes.text, classes.padSmall)}
         >
           <Trans>
-            Files is an advertising-free platform. Third-party involvement is
-            limited to facilitating the service and your connectivity to it –
-            for example, using OAuth to log into your Files account, or
-            providing financial information to pay for the service.
+            Files is an advertising-free platform. Third-party involvement is limited to facilitating the service and your connectivity to it – for example, using OAuth to log into your Files account, or providing financial information to pay for the service. The Company is not responsible for any handling of data or cookies used by third parties in facilitating the Service. 
           </Trans>
         </Typography>
 
@@ -324,11 +251,7 @@ const TermsOfServicePage: React.FC = () => {
           className={clsx(classes.text, classes.padSmall)}
         >
           <Trans>
-            ChainSafe reserves the right to review and change this agreement
-            regarding the use of the Service at any time and to notify you by
-            posting an updated version of the agreement on this Website.
-            Continued use of the Service after any such changes shall constitute
-            your consent to such changes.
+           The Company reserves the right to review and change this agreement regarding the use of the Service at any time and to notify you by posting an updated version of the agreement on this Website. Continued use of the Service after any such changes shall constitute your consent to such changes.
           </Trans>
         </Typography>
 
@@ -345,13 +268,7 @@ const TermsOfServicePage: React.FC = () => {
           className={clsx(classes.text, classes.padSmall)}
         >
           <Trans>
-            The Company does not claim any intellectual property rights over the
-            materials you store on the Service. All materials you upload remain
-            yours, and we are not responsible for damages arising from any loss
-            or depreciation of the materials. You can remove your Files
-            materials at any time by deleting your Account. You are responsible
-            for compliance of the materials with any applicable laws or
-            regulations.
+           The Company does not claim any intellectual property rights over the materials you store on the Service. All materials you upload remain yours, and the Company is not responsible for damages arising from any loss or depreciation of the materials. You can remove your Files materials at any time by deleting your Account. You are responsible for compliance of the materials with any applicable laws or regulations.
           </Trans>
         </Typography>
 
