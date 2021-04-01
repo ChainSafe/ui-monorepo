d0f5515ddbf57b9dc5def2e76b424ac795cc96b4
Ryan Noble <ryanjnoble@gmail.com>
2021-04-01 17:01:44 +0200

    Update packages/files-ui/src/Components/Modules/LoginModule/PasswordSetup.tsx
    Co-authored-by: Michael Yankelev <12774278+FSM1@users.noreply.github.com>

--- packages/files-ui/src/Components/Modules/LoginModule/PasswordSetup.tsx
+++ packages/files-ui/src/Components/Modules/LoginModule/PasswordSetup.tsx
@@ -85,7 +85,7 @@ const PasswordSetup = ({ setPassword, className, cancel }: IPasswordSetup) => {
       .string()
       .test(
         "Complexity",
-        t`Encryption password needs to be more complex`,
+        t`Password needs to be more complex`,
         async (val: string | null | undefined | object) => {
           if (val === undefined) {
             return false
