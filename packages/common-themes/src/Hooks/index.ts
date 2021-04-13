61a9189af7d9958ea18deba3874dabb960e34a4e
Thibaut Sardan <hi@thib.top>
2021-04-13 17:30:39 +0200

    fix typo

--- packages/common-themes/src/Hooks/index.ts
+++ packages/common-themes/src/Hooks/index.ts
@@ -1,6 +1,6 @@
 import { useOnClickOutside } from "./useOnClickOutside"
 import { useMediaQuery } from "./useMediaQuery"
 import { useDoubleClick } from "./useDoubleClick"
-import { default as useLocalStorage } from "./useLocalStorgage"
+import { default as useLocalStorage } from "./useLocalStorage"
 
 export { useOnClickOutside, useMediaQuery, useDoubleClick, useLocalStorage }
