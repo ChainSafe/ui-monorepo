7f1c390d36456db01d980a59e94ec233a1793253
Michael Yankelev <myankelev@gmail.com>
2021-04-26 15:15:54 +0200

    refactor initialize

--- packages/files-ui/src/Components/Modules/LoginModule/InitializeAccount.tsx
+++ packages/files-ui/src/Components/Modules/LoginModule/InitializeAccount.tsx
@@ -11,17 +11,20 @@ interface IInitializeAccount {
   className?: string
 }
 
-const InitializeAccount = ({className}: IInitializeAccount) => {
+type InitializeState =   "explainer" |
+"authenticationFactors" |
+"setUpPassword" |
+"skip" |
+"backup" |
+"complete"
+
+const Content = ({ className }: {
+  className?: string
+}) => {
   const { addPasswordShare } = useThresholdKey()
-  const [initializeState, setInitializeState] = useState<
-  "explainer" |
-  "authenticationFactors" |
-  "setUpPassword" |
-  "skip" |
-  "backup" |
-  "complete"
-  >("explainer")
   const { resetShouldInitialize } = useThresholdKey()
+
+  const [initializeState, setInitializeState] = useState<InitializeState>("explainer")
 
   const onSetPassword = useCallback((password: string) =>
     addPasswordShare(password)
@@ -31,41 +34,45 @@ const InitializeAccount = ({className}: IInitializeAccount) => {
       .catch(console.error)
   , [addPasswordShare])
 
-  return (
-    (initializeState === "explainer") ?
-      <ConciseExplainer
-        className={className}
-        onContinue={() => setInitializeState("authenticationFactors")}
-      /> :
-      (initializeState === "authenticationFactors") ?
-        <AuthenticationFactors
-          className={className}
-          goToPassword={() => setInitializeState("setUpPassword")}
-          goToMnemonic={() => setInitializeState("backup")}
-          goToSkip={() => setInitializeState("skip")}
-          goToComplete={() => setInitializeState("complete")}
-        /> :
-        (initializeState === "setUpPassword") ?
-          <PasswordSetup
-            className={className}
-            cancel={() => setInitializeState("authenticationFactors")}
-            setPassword={onSetPassword}
-          /> :
-          (initializeState === "skip") ?
-            <ConfirmSkip
-              className={className}
-              confirm={() => resetShouldInitialize()}
-              cancel={() => setInitializeState("authenticationFactors")}
-            /> :
-            (initializeState === "backup") ?
-              <SaveBackupPhrase
-                className={className}
-                cancel={() => setInitializeState("authenticationFactors")}
-                complete={() => setInitializeState("authenticationFactors")}
-              /> :
-              (initializeState === "complete") ?
-                <Complete className={className} /> : null
-  )
+
+  switch (initializeState) {
+  case "explainer":
+    return <ConciseExplainer
+      className={className}
+      onContinue={() => setInitializeState("authenticationFactors")}
+    />
+  case "authenticationFactors":
+    return <AuthenticationFactors
+      className={className}
+      goToPassword={() => setInitializeState("setUpPassword")}
+      goToMnemonic={() => setInitializeState("backup")}
+      goToSkip={() => setInitializeState("skip")}
+      goToComplete={() => setInitializeState("complete")}
+    />
+  case "setUpPassword":
+    return <PasswordSetup
+      className={className}
+      cancel={() => setInitializeState("authenticationFactors")}
+      setPassword={onSetPassword}
+    />
+  case "skip":
+    return <ConfirmSkip
+      className={className}
+      confirm={() => resetShouldInitialize()}
+      cancel={() => setInitializeState("authenticationFactors")}
+    />
+  case "backup":
+    return <SaveBackupPhrase
+      className={className}
+      cancel={() => setInitializeState("authenticationFactors")}
+      complete={() => setInitializeState("authenticationFactors")}
+    />
+  case "complete":
+    return <Complete className={className} />
+  default:
+    return null
+  }
 }
 
+const InitializeAccount = ({ className }: IInitializeAccount) => <Content className={className} />
 export default InitializeAccount
