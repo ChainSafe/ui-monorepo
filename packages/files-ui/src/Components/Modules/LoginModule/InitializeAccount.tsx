31a4bd649dc32da0c0f381d0f7a2a4faebe5084b
Michael Yankelev <12774278+FSM1@users.noreply.github.com>
2021-04-26 18:40:52 +0200

    update migrate and refactor initialize (#979)
    * update migrate and refactor initialize
    
    * lingui extract
    
    * fix lint
    
    * Update packages/files-ui/src/Components/Pages/LoginPage.tsx
    
    Co-authored-by: Ryan Noble <ryanjnoble@gmail.com>
    
    * Update packages/files-ui/src/Components/Modules/LoginModule/InitializeAccount.tsx
    
    Co-authored-by: Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
    
    * Update packages/files-ui/src/Components/Modules/LoginModule/MigrateAccount.tsx
    
    Co-authored-by: Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
    
    * refactor initialize
    
    * remove whitespace
    
    * fix lint
    
    * Update packages/files-ui/src/Components/Modules/LoginModule/InitializeAccount.tsx
    
    Co-authored-by: Tanmoy Basak Anjan <tanmoy3399@gmail.com>
    
    * Update packages/files-ui/src/Components/Modules/LoginModule/InitializeAccount.tsx
    
    Co-authored-by: Tanmoy Basak Anjan <tanmoy3399@gmail.com>
    
    * swap migrate and explainer around in migrate flow
    
    * fix lint
    
    Co-authored-by: GitHub Actions <actions@github.com>
    Co-authored-by: Ryan Noble <ryanjnoble@gmail.com>
    Co-authored-by: Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
    Co-authored-by: Tanmoy Basak Anjan <tanmoy3399@gmail.com>

---
+++ packages/files-ui/src/Components/Modules/LoginModule/InitializeAccount.tsx
@@ -0,0 +1,76 @@
+import React, { useState, useCallback } from "react"
+import { useThresholdKey } from "../../../Contexts/ThresholdKeyContext"
+import ConciseExplainer from "./ConciseExplainer"
+import Complete from "./Complete"
+import AuthenticationFactors from "./AuthenticationFactors"
+import PasswordSetup from "./PasswordSetup"
+import ConfirmSkip from "./ConfirmSkip"
+import SaveBackupPhrase from "./SaveBackupPhrase"
+
+interface IInitializeAccount {
+  className?: string
+}
+
+type InitializeState =   "explainer" |
+"authenticationFactors" |
+"setUpPassword" |
+"skip" |
+"backup" |
+"complete"
+
+const Content = ({ className }: {
+  className?: string
+}) => {
+  const { addPasswordShare, resetShouldInitialize } = useThresholdKey()
+
+  const [initializeState, setInitializeState] = useState<InitializeState>("explainer")
+
+  const onSetPassword = useCallback((password: string) =>
+    addPasswordShare(password)
+      .then(() => {
+        setInitializeState("authenticationFactors")
+      })
+      .catch(console.error)
+  , [addPasswordShare])
+
+  switch (initializeState) {
+  case "explainer":
+    return <ConciseExplainer
+      className={className}
+      onContinue={() => setInitializeState("authenticationFactors")}
+    />
+  case "authenticationFactors":
+    return <AuthenticationFactors
+      className={className}
+      goToPassword={() => setInitializeState("setUpPassword")}
+      goToMnemonic={() => setInitializeState("backup")}
+      goToSkip={() => setInitializeState("skip")}
+      goToComplete={() => setInitializeState("complete")}
+    />
+  case "setUpPassword":
+    return <PasswordSetup
+      className={className}
+      cancel={() => setInitializeState("authenticationFactors")}
+      setPassword={onSetPassword}
+    />
+  case "skip":
+    return <ConfirmSkip
+      className={className}
+      confirm={() => resetShouldInitialize()}
+      cancel={() => setInitializeState("authenticationFactors")}
+    />
+  case "backup":
+    return <SaveBackupPhrase
+      className={className}
+      cancel={() => setInitializeState("authenticationFactors")}
+      complete={() => setInitializeState("authenticationFactors")}
+    />
+  case "complete":
+    return <Complete className={className} />
+  default:
+    return null
+  }
+}
+
+const InitializeAccount = ({ className }: IInitializeAccount) => <Content className={className} />
+export default InitializeAccount
