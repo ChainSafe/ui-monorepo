2639a4b4c5f859416c3a6e960150989cd1618db1
Tanmoy Basak Anjan <tanmoy3399@gmail.com>
2021-05-05 20:17:57 +0600

    Grid view  (#999)
    * grid view tested
    
    * Update packages/files-ui/src/Components/Modules/FileBrowsers/views/FileSystemItemRow.tsx
    
    Co-authored-by: Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
    
    * translations placeholder
    
    * lingui extract
    
    * lints
    
    * removed ternary
    
    * translations
    
    * removed theme key
    
    * added toggle view button
    
    * 6 per row
    
    * moved file system item
    
    * fix max-width
    
    * added no explicit boundary rule
    
    * removed jsx types
    
    * Update packages/files-ui/src/Components/Modules/FileBrowsers/views/FileSystemItem/FileSystemItem.tsx
    
    Co-authored-by: Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
    
    * Update packages/files-ui/src/Components/Modules/FileBrowsers/views/FileSystemItem/FileSystemGridItem.tsx
    
    Co-authored-by: Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
    
    * added firefox fix
    
    * added cursor
    
    * Update packages/files-ui/src/locales/fr/messages.po
    
    Co-authored-by: Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
    
    * Update packages/files-ui/src/locales/fr/messages.po
    
    Co-authored-by: Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
    
    Co-authored-by: Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
    Co-authored-by: GitHub Actions <actions@github.com>

---
+++ packages/common-components/src/Icons/icons/Grid.icon.tsx
@@ -0,0 +1,7 @@
+import * as React from "react"
+import createSvgIcon from "../createSvgIcon"
+import { ReactComponent as GridSvg } from "../svgs/grid.svg"
+
+export { GridSvg }
+
+export default createSvgIcon(<GridSvg />)
