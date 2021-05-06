3e47c7ecc6a4ec7447611a8cff45d08107b72dd7
Thibaut Sardan <hi@thib.top>
2021-05-06 09:48:24 +0100

    cleanup

--- packages/files-ui/cypress/integration/login.ts
+++ packages/files-ui/cypress/integration/login.ts
@@ -2,9 +2,9 @@ describe("My First Test", () => {
   it("Visits the login page", () => {
     // cy.fixture("login").then((fixt) => {
     //   Object.keys(fixt).forEach((key) => {
-        // cy.window().then((window) => {
-        //   window.sessionStorage.setItem(key, fixt[key])
-        // })
+    //     cy.window().then((window) => {
+    //       window.sessionStorage.setItem(key, fixt[key])
+    //     })
     //   })
     // })
 
