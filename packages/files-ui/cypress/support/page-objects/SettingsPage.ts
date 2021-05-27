75d1a75006a273078aacdd5fc9cfcc84e505fc78
Andrew Snaith <andrew@chainsafe.io>
2021-05-27 16:22:58 -0700

    Add additional element identifiers to settings page object

--- packages/files-ui/cypress/support/page-objects/SettingsPage.ts
+++ packages/files-ui/cypress/support/page-objects/SettingsPage.ts
@@ -4,19 +4,31 @@ export class SettingsPage extends BasePage {
 
   // Elements 
   profileTabButton() {
-    return cy.get(".profile")
-  }
-
-  securityTabButton() {
-    return cy.get(".security")
-  }
-
-  securityTabHeader() {
-    return cy.get("[data-cy=settings-security-header")
+    return cy.get("[data-testid=profile-tab]")
   }
 
   profileTabHeader() {
     return cy.get("[data-cy=settings-profile-header")
+  }
+
+  FirstNameInput() {
+    return cy.get("data-cy=profile-firstname-input")
+  }
+
+  LastNameInput() {
+    return cy.get("data-cy=profile-firstname-input")
+  }
+
+  SaveChangesButton() {
+    return cy.get("data-cy=profile-save-button")
+  }
+
+  securityTabButton() {
+    return cy.get("[data-testid=security-tab]")
+  }
+
+  securityTabHeader() {
+    return cy.get("[data-cy=settings-security-header")
   }
 
   // Page helpers
