f4b8d130d4b96abe5d2d864798c57d15e83b1e52
tanmoy12 <tanmoy3399@gmail.com>
2021-06-01 00:09:19 +0600

    text input

--- packages/common-components/.storybook/contexts.js
+++ packages/common-components/.storybook/contexts.js
@@ -1,4 +1,5 @@
-import { createTheme, ThemeProvider } from "@chainsafe/common-theme"
+import { createTheme, ThemeSwitcher } from "@chainsafe/common-theme"
+import { ThemeProvider } from "@material-ui/styles"
 
 const lightTheme = createTheme()
 const darkTheme = createTheme()
@@ -7,11 +8,14 @@ export const contexts = [
   {
     icon: "box", // a icon displayed in the Storybook toolbar to control contextual props
     title: "Themes", // an unique name of a contextual environment
-    components: [ThemeProvider],
+    components: [ThemeSwitcher],
     params: [
       // an array of params contains a set of predefined `props` for `components`
-      { name: "Light Theme", props: { theme: lightTheme } },
-      { name: "Dark Theme", props: { theme: darkTheme } },
+      { name: "Light Theme", 
+        props: { 
+          themes: [lightTheme, darkTheme]
+        } 
+      },
     ],
     options: {
       deep: true,
