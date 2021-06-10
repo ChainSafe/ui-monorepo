162ef9a9a7527c31ed5b41141f671256aa5ac129
Andrew Snaith <andrew@chainsafe.io>
2021-06-09 19:43:37 -0700

    Update file management ui tests

--- packages/files-ui/cypress/tests/file-management-spec.ts
+++ packages/files-ui/cypress/tests/file-management-spec.ts
@@ -6,56 +6,65 @@ describe("File management", () => {
 
     it("can add files and cancel", () => {
       cy.web3Login()
+
+      // upload a file and see it in the file list
       homePage.uploadButton().click()
       homePage.uploadFileForm().attachFile("../fixtures/uploadedFiles/text-file.txt")
       homePage.fileUploadList().should("have.length", 1)
+
+      // cancel and ensure that the upload modal is dismissed
       homePage.uploadCancelButton().click()
       homePage.uploadCancelButton().should("not.exist")
-      cy.get("[data-cy=files-app-header").should("be.visible")
+      homePage.uploadFileForm().should("not.exist")
     })
 
-    it("can add/remove files and upload", () => {
+    it("can add/remove multiple files and upload", () => {
       cy.web3Login({ clearCSFBucket: true })
+
+      // attach 2 files to the file list
       homePage.uploadButton().click()
       homePage.uploadFileForm().attachFile("../fixtures/uploadedFiles/text-file.txt")
       homePage.fileUploadList().should("have.length", 1)
       homePage.uploadFileForm().attachFile("../fixtures/uploadedFiles/logo.png")
       homePage.fileUploadList().should("have.length", 2)
-      cy.get("[data-testid=file-list-close-button-fileUpload]").first().click()
+
+      // remove 1 file from the list and ensure 1 remains
+      homePage.fileListRemoveButton().first().click()
       homePage.fileUploadList().should("have.length", 1)
+
+      // attach an additional file to the file list and upload
       homePage.uploadFileForm().attachFile("../fixtures/uploadedFiles/text-file.txt")
       homePage.fileUploadList().should("have.length", 2)
       homePage.startUploadButton().click()
-      //cy.get("[data-cy=files-app-header]").should("be.visible")
+      homePage.uploadFileForm().should("not.exist")
       homePage.fileItemRow().should("have.length", 2)
     })
 
     it("can rename a file with error handling", () => {
       const newName = "awesome new name"
 
-      Cypress._.times(1, (k) => {
-        cy.web3Login({ clearCSFBucket: true })
+      cy.web3Login({ clearCSFBucket: true })
 
-        // add file 
-        homePage.uploadFile("../fixtures/uploadedFiles/text-file.txt")
-        homePage.fileItemRow().should("have.length", 1)
+      // upload a file 
+      homePage.uploadFile("../fixtures/uploadedFiles/text-file.txt")
+      homePage.fileItemRow().should("have.length", 1)
 
-        // ensure error is displayed if edited name is blank
-        homePage.fileItemKebabButton().first().click()
-        homePage.renameMenuOption().click()
-        homePage.fileRenameInput().type("{selectall}{del}")
-        homePage.fileRenameErrorLabel().should("be.visible")
+      // ensure an error is displayed if the edited name is blank
+      homePage.fileItemKebabButton().first().click()
+      homePage.renameMenuOption().click()
+      homePage.fileRenameInput().type("{selectall}{del}")
+      homePage.fileRenameErrorLabel().should("be.visible")
 
-        // rename a file
-        homePage.fileRenameInput().type(`${newName}{enter}`)
-        homePage.fileItemName().contains(newName)
+      // rename a file
+      homePage.fileRenameInput().type(`${newName}{enter}`)
+      homePage.fileItemName().contains(newName)
 
-        // rename file
-        homePage.fileItemKebabButton().first().click().pause()
-        homePage.renameMenuOption().click().pause()
-        homePage.fileRenameInput().type("{selectall}{del}{esc}").pause()
-        homePage.fileRenameInput().should("not.exist").pause()
-        homePage.fileItemName().contains(newName).pause()
-      })})
+      // ensure the original name persists if the rename submission is blank
+      homePage.fileItemKebabButton().first().click()
+      homePage.renameMenuOption().click()
+      homePage.fileRenameInput().type("{selectall}{del}{esc}")
+      homePage.fileRenameInput().should("not.exist")
+      homePage.fileItemName().contains(newName)
+    })
   })
 })
