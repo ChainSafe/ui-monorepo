3eb13fb09b8cb1c25db05453a4f05f9f948fb02c
Michael Yankelev <myankelev@gmail.com>
2021-06-15 17:58:38 +0200

    fix linting

--- packages/storage-ui/src/Contexts/StorageContext.tsx
+++ packages/storage-ui/src/Contexts/StorageContext.tsx
@@ -62,12 +62,13 @@ interface IFileSystemItem extends FileContentResponse {
 }
 
 type FileSystemItem = IFileSystemItem
+const REMOVE_UPLOAD_PROGRESS_DELAY = 5000
 
 const StorageContext = React.createContext<StorageContext | undefined>(undefined)
 
 const StorageProvider = ({ children }: StorageContextProps) => {
   const { storageApiClient, isLoggedIn } = useStorageApi()
-  const [spaceUsed] = useState(0)
+  const [spaceUsed, setSpaceUsed] = useState(0)
   const [pins, setPins] = useState<PinObject[]>([])
   const [storageBuckets, setStorageBuckets] = useState<Bucket[]>([])
 
@@ -118,7 +119,7 @@ const StorageProvider = ({ children }: StorageContextProps) => {
     }
   }, [isLoggedIn])
 
-  const [uploadsInProgress] = useReducer(
+  const [uploadsInProgress, dispatchUploadsInProgress] = useReducer(
     uploadsInProgressReducer,
     []
   )
