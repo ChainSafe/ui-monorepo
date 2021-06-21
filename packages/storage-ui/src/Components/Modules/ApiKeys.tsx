65ab80fc967e0992c0a15c53a4cea0286c2f3788
Michael Yankelev <myankelev@gmail.com>
2021-06-21 16:48:11 +0200

    set correct data-id

--- packages/storage-ui/src/Components/Modules/ApiKeys.tsx
+++ packages/storage-ui/src/Components/Modules/ApiKeys.tsx
@@ -120,7 +120,7 @@ const ApiKeys = () => {
         <Typography
           variant="h1"
           component="h1"
-          data-cy="storage-cids-header"
+          data-cy="storage-api-keys-header"
         >
           <Trans>
               API Keys
