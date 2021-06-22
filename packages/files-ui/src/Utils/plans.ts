ffd8e05ad6a2c1660a131773e2cb29400f25bdd2
tanmoy12 <tanmoy3399@gmail.com>
2021-06-23 02:41:58 +0600

    plans data

---
+++ packages/files-ui/src/Utils/plans.ts
@@ -0,0 +1,44 @@
+export const plans = [
+  {
+    title: "Plus",
+    for: "For individuals",
+    prices: {
+      monthly: 8,
+      yearly: 80
+    },
+    features: [
+      "All features of essential",
+      "80GB storage",
+      "Document signing",
+      "Document editor"
+    ]
+  },
+  {
+    title: "Agile",
+    for: "For individuals",
+    prices: {
+      monthly: 10,
+      yearly: 100
+    },
+    features: [
+      "All features of essential",
+      "80GB storage",
+      "Document signing",
+      "Document editor"
+    ]
+  },
+  {
+    title: "Enterprise",
+    for: "For individuals",
+    prices: {
+      monthly: 30,
+      yearly: 300
+    },
+    features: [
+      "All features of essential",
+      "80GB storage",
+      "Document signing",
+      "Document editor"
+    ]
+  }
+]
