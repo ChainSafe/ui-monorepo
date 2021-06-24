f0b70f86b8f4b6defaaca650181a3f20d3d30717
Thibaut Sardan <github@thib.top>
2021-06-24 17:04:15 +0200

    yml indentation

--- packages/storage-ui/cypress/support/page-objects/authenticationPage.ts
+++ packages/storage-ui/cypress/support/page-objects/authenticationPage.ts
@@ -6,13 +6,5 @@ export const authenticationPage = {
   // get started section elements
   web3Button: () => cy.get("[data-cy=web3]"),
   metaMaskButton: () => cy.get(".bn-onboard-modal-select-wallets > :nth-child(1) > .bn-onboard-custom"),
-  web3SignInButton: () => cy.get("[data-cy=sign-in-with-web3-button]"),
-
-  // sign in section elements
-  loginPasswordButton: () => cy.get("[data-cy=login-password-button]", { timeout: 20000 }),
-  loginPasswordInput: () => cy.get("[data-cy=login-password-input]"),
-
-  // save browser section elements
-  saveBrowserButton: () => cy.get("[data-cy=save-browser-button]"),
-  doNotSaveBrowserButton: () => cy.get("[data-cy=do-not-save-browser-button]")
+  web3SignInButton: () => cy.get("[data-cy=sign-in-with-web3-button]")
 }
