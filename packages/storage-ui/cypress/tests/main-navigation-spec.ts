13db877858fba803c1d748562718d83aafb9b1c0
Thibaut Sardan <github@thib.top>
2021-06-28 19:38:26 +0100

    back to 2min

--- packages/storage-ui/cypress/tests/main-navigation-spec.ts
+++ packages/storage-ui/cypress/tests/main-navigation-spec.ts
@@ -4,8 +4,11 @@ import { homePage } from "../support/page-objects/homePage"
 describe("Main Navigation", () => {
 
   context("desktop", () => {
-    it("can navigate to the cids page", () => {
+    before(() => {
       cy.web3Login()
+    })
+
+    it("can navigate to the cids page", () => {
       navigationMenu.cidsNavButton().click()
       cy.url().should("include", "/cids")
     })
