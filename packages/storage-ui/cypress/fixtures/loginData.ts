af0cb14393a05368edf02dc18f9569a390036e75
Andrew Snaith <andrew@chainsafe.io>
2021-06-28 12:38:44 -0700

    Try accessing app in ci with https

--- packages/storage-ui/cypress/fixtures/loginData.ts
+++ packages/storage-ui/cypress/fixtures/loginData.ts
@@ -1,4 +1,4 @@
 export const testPrivateKey = "0x4fd9a44c051834e28a919096f154e3f805f58d8f2d90a1ab9a29b0b46a6ac363"
 export const testAddress = "0xDa1f70D176E0F7F16f2a3bB509cD37Bd36aFD902"
 export const testAccountPassword = "correct horse"
-export const localHost = "http://127.0.0.1:3000"
+export const localHost = "https://localhost:3000"
