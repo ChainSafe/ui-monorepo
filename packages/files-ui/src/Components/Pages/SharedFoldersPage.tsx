41e3caa25588f9f60b67c3b7530e6a8d9a032414
tanmoy12 <tanmoy3399@gmail.com>
2021-06-28 22:43:59 +0600

    using shared page

---
+++ packages/files-ui/src/Components/Pages/SharedFoldersPage.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import SharedFolderOverview from "../Modules/FileBrowsers/SharedFoldersOverview"
+
+const SharedFoldersPage = () => {
+  return <SharedFolderOverview />
+}
+
+export default SharedFoldersPage
