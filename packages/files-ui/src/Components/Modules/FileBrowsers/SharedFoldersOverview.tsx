af901735ee03b525481e228925fa95b8ebc79fcc
Michael Yankelev <12774278+FSM1@users.noreply.github.com>
2021-07-01 17:25:21 +0300

    Create shared folder modal  (#1201)
    * wire up create share in context
    
    * wire up create share modal
    
    * make eslint :)
    
    * add translations
    
    * lingui extract
    
    * fix callback
    
    * fix buttons container
    
    * remove unused import
    
    * make eslint :D
    
    * hide dropdown and separator
    
    * Apply suggestions from code review
    
    Co-authored-by: Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
    
    * lingui extract
    
    * resolve feedback
    
    * lingui extract
    
    * rename type and fix regex
    
    * lowercase address
    
    * rename file to match component
    
    * make sure sharedFolderUsers is an array before mapping
    
    * Apply suggestions from code review
    
    Co-authored-by: Tanmoy Basak Anjan <tanmoy3399@gmail.com>
    Co-authored-by: Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
    
    * update styling
    
    * prevent owner from adding himself
    
    * revert to labels
    
    * resolve duplicate key
    
    * add validation to form
    
    * clear form contents
    
    * make LGTM :D
    
    Co-authored-by: GitHub Actions <actions@github.com>
    Co-authored-by: Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
    Co-authored-by: Tanmoy Basak Anjan <tanmoy3399@gmail.com>

--- packages/files-ui/src/Components/Modules/FileBrowsers/SharedFoldersOverview.tsx
+++ packages/files-ui/src/Components/Modules/FileBrowsers/SharedFoldersOverview.tsx
@@ -8,15 +8,17 @@ import {
   TableBody,
   SortDirection,
   Loading,
+  Button,
+  PlusIcon,
   useHistory
 } from "@chainsafe/common-components"
 import { useFiles } from "../../../Contexts/FilesContext"
 import { Trans } from "@lingui/macro"
 import { createStyles, makeStyles, useThemeSwitcher } from "@chainsafe/common-theme"
 import { CSFTheme } from "../../../Themes/types"
-import { useFilesApi } from "../../../Contexts/FilesApiContext"
 import SharedFolderRowWrapper from "./SharedFolderRowWrapper"
 import clsx from "clsx"
+import CreateSharedFolderModal from "./CreateSharedFolderModal"
 import { ROUTE_LINKS } from "../../FilesRoutes"
 
 export const desktopSharedGridSettings = "69px 3fr 190px 150px 45px !important"
@@ -95,8 +97,8 @@ const useStyles = makeStyles(
 
 const SharedFolderOverview = () => {
   const classes = useStyles()
-  const { buckets, refreshBuckets, isLoadingBuckets } = useFiles()
-  const { filesApiClient, encryptedEncryptionKey } = useFilesApi()
+  const { buckets, isLoadingBuckets } = useFiles()
+  const [createSharedFolderModalOpen, setCreateSharedFolderModalOpen] = useState(false)
   const [direction, setDirection] = useState<SortDirection>("ascend")
   const [column, setColumn] = useState<"name" | "size" | "date_uploaded">("name")
   const { redirect } = useHistory()
@@ -124,73 +126,65 @@ const SharedFolderOverview = () => {
   }, [redirect])
 
   return (
-    <article
-      className={classes.root}
-    >
-      <header className={classes.header}>
-        <Typography
-          variant="h1"
-          component="h1"
-          data-cy="shared-overview-header"
-        >
-          <Trans>Shared folders</Trans>
-        </Typography>
-        <div className={classes.controls}>
-        </div>
-      </header>
-      <button
-        onClick={() => {
-          !!encryptedEncryptionKey && filesApiClient.createBucket({
-            name: `Cat Bucket ${Date.now()}`,
-            encryption_key: encryptedEncryptionKey,
-            type: "share"
-          }).then((res) => {
-            console.log(res)
-            refreshBuckets()
-          })
-            .catch(console.error)
-        }}>
-        Create a shared &quot;Cat Bucket&quot;
-      </button>
-      {isLoadingBuckets && (
-        <div
-          className={clsx(classes.loadingContainer)}
-        >
-          <Loading size={24}
-            type="light" />
-          <Typography variant="body2"
-            component="p">
-            <Trans>Loading your shared folders…</Trans>
+    <>
+      <article
+        className={classes.root}
+      >
+        <header className={classes.header}>
+          <Typography
+            variant="h1"
+            component="h1"
+            data-cy="shared-overview-header"
+          >
+            <Trans>Shared folders</Trans>
           </Typography>
-        </div>
-      )}
-      {!isLoadingBuckets && (
-        <Table
-          fullWidth={true}
-          striped={true}
-          hover={true}
-        >
-          <TableHead className={classes.tableHead}>
-            <TableRow type="grid"
-              className={classes.tableRow}>
-              {desktop &&
+          <div className={classes.controls}>
+            <Button variant='outline'
+              onClick={() => setCreateSharedFolderModalOpen(true)}>
+              <PlusIcon />
+              <Trans>Create a Shared Folder</Trans>
+            </Button>
+          </div>
+        </header>
+        {isLoadingBuckets && (
+          <div
+            className={clsx(classes.loadingContainer)}
+          >
+            <Loading size={24}
+              type="light" />
+            <Typography variant="body2"
+              component="p">
+              <Trans>Loading your shared folders…</Trans>
+            </Typography>
+          </div>
+        )}
+        {!isLoadingBuckets && (
+          <Table
+            fullWidth={true}
+            striped={true}
+            hover={true}
+          >
+            <TableHead className={classes.tableHead}>
+              <TableRow type="grid"
+                className={classes.tableRow}>
+                {desktop &&
                 <TableHeadCell>
                   {/* Icon */}
                 </TableHeadCell>
-              }
-              <TableHeadCell
-                sortButtons={true}
-                align="left"
-                onSortChange={() => handleSortToggle("name")}
-                sortDirection={column === "name" ? direction : undefined}
-                sortActive={column === "name"}
-              >
-                <Trans>Name</Trans>
-              </TableHeadCell>
-              <TableHeadCell align="left">
-                <Trans>Shared with</Trans>
-              </TableHeadCell>
-              {desktop &&
+                }
+                <TableHeadCell
+                  sortButtons={true}
+                  align="left"
+                  onSortChange={() => handleSortToggle("name")}
+                  sortDirection={column === "name" ? direction : undefined}
+                  sortActive={column === "name"}
+                >
+                  <Trans>Name</Trans>
+                </TableHeadCell>
+                <TableHeadCell align="left">
+                  <Trans>Shared with</Trans>
+                </TableHeadCell>
+                {desktop &&
                 <TableHeadCell
                   sortButtons={true}
                   align="left"
@@ -200,22 +194,26 @@ const SharedFolderOverview = () => {
                 >
                   <Trans>Size</Trans>
                 </TableHeadCell>
-              }
-              <TableHeadCell>{/* Menu */}</TableHeadCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {bucketsToShow.map((bucket) =>
-              <SharedFolderRowWrapper
-                key={bucket.id}
-                bucket={bucket}
-                openSharedFolder={openSharedFolder}
-              />
-            )}
-          </TableBody>
-        </Table>
-      )}
-    </article>
+                }
+                <TableHeadCell>{/* Menu */}</TableHeadCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {bucketsToShow.map((bucket) =>
+                <SharedFolderRowWrapper
+                  key={bucket.id}
+                  bucket={bucket}
+                  openSharedFolder={openSharedFolder}
+                />
+              )}
+            </TableBody>
+          </Table>
+        )}
+      </article>
+      <CreateSharedFolderModal
+        modalOpen={createSharedFolderModalOpen}
+        close={() => setCreateSharedFolderModalOpen(false)}/>
+    </>
   )
 }
 
