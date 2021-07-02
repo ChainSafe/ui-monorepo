b9e368f33da81974d5969be982a69d2e4c9d2cba
tanmoy12 <tanmoy3399@gmail.com>
2021-07-02 18:29:57 +0600

    owner column

--- packages/files-ui/src/Components/Modules/FileBrowsers/views/FileSystemItem/SharedFolderRow.tsx
+++ packages/files-ui/src/Components/Modules/FileBrowsers/views/FileSystemItem/SharedFolderRow.tsx
@@ -218,7 +218,7 @@ const SharedFolderRow = ({ bucket, onFolderClick, menuItems, isEditing, setIsEdi
       </TableCell>
       {desktop &&
         <TableCell align="left">
-
+          {isOwner && t`me`}
         </TableCell>
       }
       <TableCell
