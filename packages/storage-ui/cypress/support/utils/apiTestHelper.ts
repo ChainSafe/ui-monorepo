3b537087865a4e1505f9cf4aa1e5227bdfaf6e5a
Michael Yankelev <myankelev@gmail.com>
2021-07-06 12:24:26 +0300

    fix test login

--- packages/storage-ui/cypress/support/utils/apiTestHelper.ts
+++ packages/storage-ui/cypress/support/utils/apiTestHelper.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { FilesApiClient } from "@chainsafe/files-api-client"
 
-const REFRESH_TOKEN_KEY = "csf.refreshToken"
+const REFRESH_TOKEN_KEY = "css.refreshToken"
 
 export const apiTestHelper = {
   clearPins(apiUrlBase: string) {
@@ -13,12 +13,14 @@ export const apiTestHelper = {
     cy.window().then((win) => {
       apiClient
         .getRefreshToken({
-          refresh: win.sessionStorage.getItem(REFRESH_TOKEN_KEY) || ""
+          refresh: win.localStorage.getItem(REFRESH_TOKEN_KEY) || ""
         })
         .then((tokens) => {
           apiClient.setToken(tokens.access_token.token)
           apiClient.listPins(undefined, undefined, ["queued", "pinning", "pinned", "failed"])
-            .then((pins) => pins.results?.forEach(ps => apiClient.deletePin(ps.requestid)))
+            .then((pins) => 
+              pins.results?.forEach(ps => apiClient.deletePin(ps.requestid)
+            ))
         })
     })
   }
