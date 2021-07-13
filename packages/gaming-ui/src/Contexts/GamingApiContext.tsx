e7fb2426daac950af09aabaaf9b9e90c8f268cad
Michael Yankelev <myankelev@gmail.com>
2021-07-13 16:15:29 +0300

    remove unused imports

--- packages/gaming-ui/src/Contexts/GamingApiContext.tsx
+++ packages/gaming-ui/src/Contexts/GamingApiContext.tsx
@@ -75,8 +75,8 @@ const GamingApiProvider = ({ apiUrl, withLocalStorage = true, children }: Gaming
   const maintenanceMode = process.env.REACT_APP_MAINTENANCE_MODE === "true"
 
   const { wallet, onboard, checkIsReady, isReady, provider, address } = useWeb3()
-  const { localStorageRemove, localStorageGet, localStorageSet } = useLocalStorage()
-  const { sessionStorageRemove, sessionStorageGet, sessionStorageSet } = useSessionStorage()
+  const { localStorageRemove, localStorageSet } = useLocalStorage()
+  const { sessionStorageRemove, sessionStorageSet } = useSessionStorage()
 
   // initializing api
   const initialAxiosInstance = useMemo(() => axios.create({
