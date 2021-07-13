2de4dc587a8bd13ff887abbc75a830d1a16b41cd
Michael Yankelev <12774278+FSM1@users.noreply.github.com>
2021-07-13 21:56:16 +0300

    Barebones gaming app (#1256)
    * create barebones gaming app
    
    * lingui extract
    
    * update readme
    
    * Apply suggestions from code review
    
    Co-authored-by: Ryan Noble <ryanjnoble@gmail.com>
    
    * lingui extract
    
    * rename
    
    Co-authored-by: GitHub Actions <actions@github.com>
    Co-authored-by: Ryan Noble <ryanjnoble@gmail.com>

---
+++ packages/gaming-ui/cypress/plugins/index.ts
@@ -0,0 +1,57 @@
+/// <reference types="cypress" />
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+import { existsSync, readFileSync } from "fs"
+
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+
+export default (on: any) => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+
+  on("task", {
+    readFileMaybe(filename: string) {
+      if (existsSync(filename)) {
+        return readFileSync(filename, "utf8")
+      }
+
+      return null
+    }
+  })
+
+  on("before:browser:launch", (browser: Cypress.Browser, launchOptions: Cypress.BrowserLaunchOptions) => {
+    if (browser.name === "chrome" && browser.isHeadless) {
+      // fullPage screenshot size is 1280x720 on non-retina screens
+      launchOptions.args.push("--window-size=1280,720")
+
+      // force screen to be non-retina (1280x720 size)
+      launchOptions.args.push("--force-device-scale-factor=1")
+    }
+
+    if (browser.name === "electron" && browser.isHeadless) {
+      // fullPage screenshot size is 12807200
+      launchOptions.preferences.width = 1280
+      launchOptions.preferences.height = 720
+    }
+
+    if (browser.name === "firefox" && browser.isHeadless) {
+      // menubars take up height on the screen
+      launchOptions.args.push("--width=1280")
+      launchOptions.args.push("--height=720")
+    }
+
+    return launchOptions
+  })
+}
---
+++ packages/gaming-ui/cypress/support/page-objects/basePage.ts
@@ -0,0 +1,6 @@
+// Only add things here that could be applicable to all / most pages
+
+export const basePage = {
+  // Mobile view only element
+  hamburgerMenuButton: () => cy.get("[data-testId=hamburger-menu]")
+}
