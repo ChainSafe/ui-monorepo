56adec7449d0a69c4495b514e9b5c43f567c75e2
Thibaut Sardan <github@thib.top>
2021-07-29 17:29:54 +0200

    remove duplicate

--- packages/common-components/src/Icons/icons/UserGroup.icon.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react"
-import createSvgIcon from "../createSvgIcon"
-import { ReactComponent as UserGroupSvg } from "../svgs/user-group.svg"
-
-export { UserGroupSvg }
-
-export default createSvgIcon(<UserGroupSvg />)
