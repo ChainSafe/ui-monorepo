aa0b1a410da89c58e427b53e1e311f7bb5c26eac
Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
2021-08-05 18:45:21 +0200

    Folder creation validation and test (#1416)

--- packages/files-ui/cypress/tests/file-management-spec.ts
+++ packages/files-ui/cypress/tests/file-management-spec.ts
@@ -181,5 +181,35 @@ describe("File management", () => {
       binPage.fileItemRow().should("have.length", 1)
       homePage.fileItemName().should("have.text", folderName)
     })
+
+    it("cannot create a folder with an invalid name", () => {
+      cy.web3Login({ clearCSFBucket: true, clearTrashBucket: true })
+      homePage.newFolderButton().click()
+
+      // ensure a folder can't be created without entering a name
+      homePage.createButton().should("have.attr", "disabled")
+      homePage.folderNameInput().type("a{selectall}{del}")
+      homePage.folderCreationErrorLabel().should("be.visible")
+
+      // ensure a folder can't be created with "/" in the name
+      homePage.folderNameInput().type("/")
+      homePage.createButton().should("have.attr", "disabled")
+      homePage.folderCreationErrorLabel().should("be.visible")
+      homePage.createFolderModal().should("contain.text", "A name cannot contain '/' character")
+
+      // ensure a folder can't be created with white space only in the name
+      homePage.folderNameInput().type("{selectall}{del}")
+      homePage.folderNameInput().type("   ")
+      homePage.createButton().should("have.attr", "disabled")
+      homePage.folderCreationErrorLabel().should("be.visible")
+      homePage.createFolderModal().should("contain.text", "Please enter a name")
+
+      // ensure a folder can't be created if name exceeds 65 characters
+      homePage.folderNameInput().type("{selectall}{del}")
+      homePage.folderNameInput().type("cgsxffymqivoknhwhqvmnchvjngtwsriovhvkgzgmonmimctcrdytujbtkogngvext")
+      homePage.createButton().should("have.attr", "disabled")
+      homePage.folderCreationErrorLabel().should("be.visible")
+      homePage.createFolderModal().should("contain.text", "Name too long")
+    })
   })
 })
