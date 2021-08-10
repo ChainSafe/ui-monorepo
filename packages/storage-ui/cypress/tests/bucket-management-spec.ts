28d7e19321104ee0e9b7d5d39fdb1472ca778b1c
Andrew Snaith <andrew@chainsafe.io>
2021-08-10 13:56:58 -0700

    Update bucket tests

--- packages/storage-ui/cypress/tests/bucket-management-spec.ts
+++ packages/storage-ui/cypress/tests/bucket-management-spec.ts
@@ -7,15 +7,16 @@ describe("Bucket management", () => {
 
   context("desktop", () => {
 
-    it.skip("can create a bucket", () => {
+    it("can create a bucket", () => {
       cy.web3Login({ clearPins: true })
 
       // create a bucket and see it in the bucket table
       navigationMenu.bucketsNavButton().click()
       bucketsPage.createBucketButton().click()
       bucketsPage.bucketNameInput().type(bucketName)
-      bucketsPage.createBucketSubmitButton().click()
+      bucketsPage.createBucketSubmitButton().safeClick()
       bucketsPage.bucketItemRow().should("have.length", 1)
+      bucketsPage.bucketItemName().should("have.text", bucketName)
 
       // open create bucket modal and cancel it
       bucketsPage.createBucketButton().click()
@@ -27,11 +28,12 @@ describe("Bucket management", () => {
       cy.web3Login({ clearPins: true })
 
       // delete a bucket and ensure it's row is removed
-      navigationMenu.bucketsNavButton().click()
+      navigationMenu.bucketsNavButton().click().pause()
       bucketsPage.createBucket(bucketName)
       bucketsPage.bucketRowKebabButton().first().click()
       bucketsPage.deleteBucketMenuOption().first().click()
-      bucketsPage.bucketItemRow().should("not.exist")
+      bucketsPage.bucketItemRow().should("not.be.visible")
+      bucketsPage.bucketItemName().should("not.be.visible")
     })
   })
 })
