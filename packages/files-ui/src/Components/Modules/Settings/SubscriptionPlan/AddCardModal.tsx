8f0700d71f106ff1541b5d6747b8976f25fddf63
tanmoy12 <tanmoy3399@gmail.com>
2021-08-16 21:25:30 +0600

    billing add card

--- packages/files-ui/src/Components/Modules/Settings/SubscriptionPlan/AddCardModal.tsx
+++ packages/files-ui/src/Components/Modules/Settings/SubscriptionPlan/AddCardModal.tsx
@@ -2,21 +2,23 @@ import {
   Button,
   Grid,
   TextInput,
-  Typography
+  Typography,
+  useToaster
 } from "@chainsafe/common-components"
 import {
   createStyles,
   makeStyles
 } from "@chainsafe/common-theme"
 import React, { useState } from "react"
-import { Formik, Form } from "formik"
 import { CSFTheme } from "../../../../Themes/types"
 import CustomModal from "../../../Elements/CustomModal"
 import CustomButton from "../../../Elements/CustomButton"
 import { t, Trans } from "@lingui/macro"
 import CardInputs from "../../../Elements/CardInputs"
 import { getCardNumberError, getCVCError, getExpiryDateError } from "../../../Elements/CardInputs/utils/validator"
-
+import axios, { AxiosResponse } from "axios"
+import qs from "qs"
+import { useFilesApi } from "../../../../Contexts/FilesApiContext"
 
 const useStyles = makeStyles(
   ({ breakpoints, constants, typography, zIndex }: CSFTheme) => {
@@ -86,18 +88,62 @@ const CreateFolderModal: React.FC<ICreateFolderModalProps> = ({
   })
   const [cardName, setCardName] = useState("")
   const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+  const { filesApiClient } = useFilesApi()
+  const { addToastMessage } = useToaster()
 
-  const onSubmitCard = () => {
+  const onCloseModal = () => {
+    setCardInputs({ cardNumber: "", cardExpiry: "", cardCvc: "" })
+    setCardName("")
     setError("")
+    onClose()
+  }
+
+  const onSubmitCard = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
     const error = !cardName
-      ? "Name on card is required"
+      ? t`Name on card is required`
       : getCardNumberError(cardInputs.cardNumber) ||
       getExpiryDateError(cardInputs.cardExpiry) ||
       getCVCError(cardInputs.cardCvc, cardInputs.cardNumber)
+
     if (error) {
       setError(error)
       return
     }
+    setError("")
+
+    setLoading(true)
+    // get token from stripe
+    axios({
+      method: "post",
+      url: "https://api.stripe.com/v1/tokens",
+      headers: {
+        "Authorization": `Bearer ${process.env.REACT_APP_STRIPE_PK}`,
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      data : qs.stringify({
+        "card[number]": cardInputs.cardNumber,
+        "card[exp_month]": cardInputs.cardExpiry.split("/")[0].trim(),
+        "card[exp_year]": cardInputs.cardExpiry.split("/")[1].trim(),
+        "card[cvc]": cardInputs.cardCvc
+      })
+    }).then((resp: AxiosResponse<{ card: {id: string}}>) => {
+      // send stripe token to API
+      filesApiClient.addCard({ token: resp.data.card.id })
+        .then(() => {
+          onCloseModal()
+          addToastMessage({
+            message: t`Card added successfully`
+          })
+        }).catch(() => {
+          setError(t`Something went wrong`)
+        }).finally(() => setLoading(false))
+    }).catch((err) => {
+      console.error(err)
+      setError(t`Card details could not be validated`)
+      setLoading(false)
+    })
   }
 
   return (
@@ -110,88 +156,80 @@ const CreateFolderModal: React.FC<ICreateFolderModalProps> = ({
       closePosition="none"
       maxWidth="sm"
     >
-      <Formik
-        initialValues={{
-          name: ""
-        }}
-        validateOnChange={false}
-        onSubmit={async () => {
-          // 
-        }}
-      >
-        <Form>
-          <div
-            className={classes.root}
-            data-cy="modal-create-folder"
+      <form onSubmit={onSubmitCard}>
+        <div
+          className={classes.root}
+          data-cy="modal-create-folder"
+        >
+          <Grid
+            item
+            xs={12}
+            sm={12}
           >
-            <Grid
-              item
-              xs={12}
-              sm={12}
+            <Typography
+              className={classes.heading}
+              variant="h4"
+              component="h4"
             >
-              <Typography
-                className={classes.heading}
-                variant="h4"
-                component="h4"
-              >
-                <Trans>Add a credit card</Trans>
-              </Typography>
-            </Grid>
-            <Grid
-              item
-              xs={12}
-              sm={12}
+              <Trans>Add a credit card</Trans>
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={12}
+          >
+            <TextInput
+              value={cardName}
+              onChange={(val) =>
+                setCardName(val?.toString() || "")
+              }
+              // className={classes.cardNumber}
+              size="large"
+              placeholder={t`Name on card`}
+              label={t`Name on card`}
+            />
+            <CardInputs
+              cardNumber={cardInputs.cardNumber}
+              cardExpiry={cardInputs.cardExpiry}
+              cardCvc={cardInputs.cardCvc}
+              handleChangeCardNumber={(value) => setCardInputs({ ...cardInputs, cardNumber: value })}
+              handleChangeCardExpiry={(value) => setCardInputs({ ...cardInputs, cardExpiry: value })}
+              handleChangeCardCvc={(value) => setCardInputs({ ...cardInputs, cardCvc: value })}
+              error={error}
+            />
+          </Grid>
+          <Grid
+            item
+            flexDirection="row"
+            justifyContent="flex-end"
+            className={classes.footer}
+          >
+            <CustomButton
+              data-cy="button-cancel-create-folder"
+              onClick={onCloseModal}
+              size="medium"
+              className={classes.cancelButton}
+              variant="outline"
+              type="button"
+              disabled={loading}
             >
-              <TextInput
-                value={cardName}
-                onChange={(val) =>
-                  setCardName(val?.toString() || "")
-                }
-                // className={classes.cardNumber}
-                size="large"
-                placeholder={t`Name on card`}
-                label={t`Name on card`}
-              />
-              <CardInputs
-                cardNumber={cardInputs.cardNumber}
-                cardExpiry={cardInputs.cardExpiry}
-                cardCvc={cardInputs.cardCvc}
-                handleChangeCardNumber={(value) => setCardInputs({ ...cardInputs, cardNumber: value })}
-                handleChangeCardExpiry={(value) => setCardInputs({ ...cardInputs, cardExpiry: value })}
-                handleChangeCardCvc={(value) => setCardInputs({ ...cardInputs, cardCvc: value })}
-                error={error}
-              />
-            </Grid>
-            <Grid
-              item
-              flexDirection="row"
-              justifyContent="flex-end"
-              className={classes.footer}
+              <Trans>Cancel</Trans>
+            </CustomButton>
+            <Button
+              data-cy="button-create-folder"
+              size="medium"
+              variant="primary"
+              type="submit"
+              className={classes.okButton}
+              loading={loading}
+              disabled={loading}
             >
-              <CustomButton
-                data-cy="button-cancel-create-folder"
-                onClick={() => onClose()}
-                size="medium"
-                className={classes.cancelButton}
-                variant="outline"
-                type="button"
-              >
-                <Trans>Cancel</Trans>
-              </CustomButton>
-              <Button
-                data-cy="button-create-folder"
-                size="medium"
-                variant="primary"
-                type="submit"
-                className={classes.okButton}
-                onClick={onSubmitCard}
-              >
-                <Trans>Add card</Trans>
-              </Button>
-            </Grid>
-          </div>
-        </Form>
-      </Formik>
+              <Trans>Add card</Trans>
+            </Button>
+          </Grid>
+        </div>
+      </form>
     </CustomModal>
   )
 }
