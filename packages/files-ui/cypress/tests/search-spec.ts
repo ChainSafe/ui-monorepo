805ba29c12fb38a2020d3227cd64a29c30da41e0
Andrew Snaith <andrew@chainsafe.io>
2021-08-30 14:02:22 -0700

    Implement pr feedback on search spec file

--- packages/files-ui/cypress/tests/search-spec.ts
+++ packages/files-ui/cypress/tests/search-spec.ts
@@ -5,6 +5,9 @@ import { apiTestHelper } from "../support/utils/apiTestHelper"
 
 describe("Search", () => {
 
+  const folderA = `${folderName}A`
+  const folderB = `${folderName}B`
+
   context("desktop", () => {
 
     it("can search for files and folders via name", () => {
@@ -13,38 +16,42 @@ describe("Search", () => {
       // add file and folders
       homePage.uploadFile("../fixtures/uploadedFiles/logo.png")
       homePage.fileItemName().invoke("text").as("fileName")
-      apiTestHelper.createFolder(`/${folderName}A`)
-      apiTestHelper.createFolder(`/${folderName}B`)
+      apiTestHelper.createFolder("/" + folderA)
+      apiTestHelper.createFolder("/" + folderB)
 
       // search for a specific folder, ensure 1 result is found
-      homePage.searchInput().type(`${folderName}A{enter}`)
-      searchPage.fileItemRow().should("be.visible")
+      homePage.searchInput().type(`${folderA}{enter}`)
+      searchPage.fileItemRow()
+        .should("be.visible")
         .should("have.length", 1)
-      searchPage.fileItemName().should("contain.text", `${folderName}A`)
+      searchPage.fileItemName().should("contain.text", folderA)
       cy.go("back")
 
       // perform a loose search for folders, ensure that 2 results are found
       homePage.searchInput().type(`{selectall}{del}${folderName}{enter}`)
-      searchPage.fileItemRow().should("be.visible")
+      searchPage.fileItemRow()
+        .should("be.visible")
         .should("have.length", 2)
-      searchPage.fileItemName().should("contain.text", `${folderName}A`)
-      searchPage.fileItemName().should("contain.text", `${folderName}B`)
+      searchPage.fileItemName().should("contain.text", folderA)
+      searchPage.fileItemName().should("contain.text", folderB)
       cy.go("back")
 
       // search for a specific file, ensure only 1 result is found
       cy.get("@fileName").then(($fileName) => {
         homePage.searchInput().type(`{selectall}{del}${$fileName}{enter}`)
-        searchPage.fileItemRow().should("be.visible")
+        searchPage.fileItemRow()
+          .should("be.visible")
           .should("have.length", 1)
-        searchPage.fileItemName().should("contain.text", `${$fileName}`)
+        searchPage.fileItemName().should("contain.text", $fileName)
       })
     })
 
     it("can see no data state when no search results are found", () => {
       cy.web3Login({ clearCSFBucket: true, clearTrashBucket: true })
-      homePage.searchInput().type(`{selectall}{del}${folderName}C{enter}`)
+      homePage.searchInput().type(`{selectall}{del}${folderName}{enter}`)
       searchPage.appHeaderLabel().should("have.text", "Search results")
-      searchPage.noDataStateInfo().should("be.visible")
+      searchPage.noDataStateInfo()
+        .should("be.visible")
         .should("exist")
     })
 
@@ -56,12 +63,12 @@ describe("Search", () => {
       // view contents via menu option
       searchPage.fileItemKebabButton().click()
       searchPage.viewFolderMenuOption().click()
-      searchPage.folderBreadcrumb().should("have.text", `${folderName}`)
+      searchPage.folderBreadcrumb().should("have.text", folderName)
       cy.go("back")
 
       // view contents via double click
-      searchPage.fileItemRow().contains(`${folderName}`).dblclick()
-      searchPage.folderBreadcrumb().should("have.text", `${folderName}`)
+      searchPage.fileItemRow().contains(folderName).dblclick()
+      searchPage.folderBreadcrumb().should("have.text", folderName)
     })
   })
 })
