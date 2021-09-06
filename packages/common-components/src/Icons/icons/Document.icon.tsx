b2cc760ff42b8f22f44c9921be773d82ff1246da
ryry79261 <ryanjnoble@gmail.com>
2021-09-06 14:01:08 +0200

    Added doc link

---
+++ packages/common-components/src/Icons/icons/Document.icon.tsx
@@ -0,0 +1,7 @@
+import * as React from "react"
+import createSvgIcon from "../createSvgIcon"
+import { ReactComponent as DocumentSvg } from "../svgs/document.svg"
+
+export { DocumentSvg }
+
+export default createSvgIcon(<DocumentSvg />)
