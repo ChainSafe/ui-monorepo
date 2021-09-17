bdcd6a90c704b4025809b5241215182ed9e1b8f7
Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
2021-09-17 12:53:03 +0100

    Don't track before opt-in (#1531)
    * dont track users before they opt-in
    
    * nits and remove log, calls are still fires
    
    * default to not track
    
    * lingui extract
    
    * with init check
    
    * Update packages/files-ui/src/Contexts/PosthogContext.tsx
    
    Co-authored-by: Michael Yankelev <12774278+FSM1@users.noreply.github.com>
    
    * lingui extract
    
    Co-authored-by: GitHub Actions <actions@github.com>
    Co-authored-by: Michael Yankelev <12774278+FSM1@users.noreply.github.com>

--- packages/files-ui/src/index.tsx
+++ packages/files-ui/src/index.tsx
@@ -9,7 +9,8 @@ if (process.env.REACT_APP_POSTHOG_PROJECT_API_KEY &&
     process.env.REACT_APP_POSTHOG_INSTANCE_ADDRESS)
 {
   posthog.init(process.env.REACT_APP_POSTHOG_PROJECT_API_KEY, {
-    api_host: process.env.REACT_APP_POSTHOG_INSTANCE_ADDRESS
+    api_host: process.env.REACT_APP_POSTHOG_INSTANCE_ADDRESS,
+    opt_out_capturing_by_default: true
   })
 }
 
