372716e428f2e393bb3b9c4762cae21f518e3d8c
Michael Yankelev <myankelev@gmail.com>
2021-09-28 14:48:45 +0200

    fix effect deps

--- packages/files-ui/src/Contexts/PosthogContext.tsx
+++ packages/files-ui/src/Contexts/PosthogContext.tsx
@@ -5,7 +5,7 @@ import { createStyles, makeStyles } from "@chainsafe/common-theme"
 import { Trans } from "@lingui/macro"
 import { useLocalStorage } from "@chainsafe/browser-storage-hooks"
 import { CSFTheme } from "../Themes/types"
-import {useUser} from "./UserContext"
+import { useUser } from "./UserContext"
 
 export type PosthogContext = {
   hasOptedIn: boolean
@@ -74,7 +74,7 @@ const PosthogProvider = ({ children }: PosthogProviderProps) => {
   const [showBanner, setShowBanner] = useState(false)
   const [hasTouchedCookieBanner, setHasTouchedCookieBanner ] = useState(false)
   const { localStorageGet, localStorageSet } = useLocalStorage()
-  const {profile} = useUser()
+  const { profile } = useUser()
 
   const classes = useStyles()
   const posthogInitialized = useMemo(() =>
@@ -122,7 +122,7 @@ const PosthogProvider = ({ children }: PosthogProviderProps) => {
     } else {
       posthogInitialized && posthog.reset()
     }
-  }, [profile])
+  }, [profile, posthogInitialized])
 
   return (
     <PosthogContext.Provider
