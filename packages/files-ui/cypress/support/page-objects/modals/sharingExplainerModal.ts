56c0966ac9d084d819b8e88c929a6b2841355b1b
Andrew Snaith <andrew@chainsafe.io>
2021-10-25 20:20:33 -0700

    fix mistake in identifiers

--- packages/files-ui/cypress/support/page-objects/modals/sharingExplainerModal.ts
+++ packages/files-ui/cypress/support/page-objects/modals/sharingExplainerModal.ts
@@ -1,5 +1,5 @@
 export const sharingExplainerModal = {
   body: () => cy.get("[data-testId=modal-container-sharing-explainer]"),
-  gotItButton: () => cy.get("[data-testId=button-got-it]"),
-  nextButton: () => cy.get("[data-testId=button-next]")
+  gotItButton: () => cy.get("[data-cy=button-got-it]"),
+  nextButton: () => cy.get("[data-cy=button-next]")
 }
