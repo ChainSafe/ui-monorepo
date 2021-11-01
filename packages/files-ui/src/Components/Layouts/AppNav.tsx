626ef2ce4411b1f8a1316542ad1d35791b73c19e
Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
2021-11-01 18:29:22 +0000

    Update AppNav.tsx (#1689)

--- packages/files-ui/src/Components/Layouts/AppNav.tsx
+++ packages/files-ui/src/Components/Layouts/AppNav.tsx
@@ -25,7 +25,7 @@ const useStyles = makeStyles(
     return createStyles({
       root: {
         width: 0,
-        overflow: "scroll",
+        overflow: "auto",
         transitionDuration: `${animation.translate}ms`,
         display: "flex",
         flexDirection: "column",
