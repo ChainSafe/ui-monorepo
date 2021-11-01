70bcd058629c16d76204e6370c8e4904b9e45a20
Andrew Snaith <andrew@chainsafe.io>
2021-11-01 16:39:46 -0700

    move data to test fixture

--- packages/files-ui/cypress/fixtures/filesTestData.ts
+++ packages/files-ui/cypress/fixtures/filesTestData.ts
@@ -1,3 +1,5 @@
 export const folderName = "Group"
 export const folderPath = `/${folderName}`
 export const profileCreatedDate = "2021-05-20T21:26:36.598924Z"
+export const sharingExplainerKey = "csf.dismissedSharingExplainer"
+export const sharedFolderName = "Share"
