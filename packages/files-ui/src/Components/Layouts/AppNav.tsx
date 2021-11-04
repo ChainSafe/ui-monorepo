eae4816c6e0abcab2081f06d94e33d7609a20916
Michael Yankelev <myankelev@gmail.com>
2021-11-04 23:55:26 +0200

    fix lint

--- packages/files-ui/src/Components/Layouts/AppNav.tsx
+++ packages/files-ui/src/Components/Layouts/AppNav.tsx
@@ -169,7 +169,7 @@ const useStyles = makeStyles(
           "& svg": {
             fill: constants.nav.itemIconColorHover
           }
-        },
+        }
       },
       navItemText: {
         [breakpoints.down("md")]: {
