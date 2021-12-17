65ebd2fd944dea6493135a1ec35e9d2e50841381
Thibaut Sardan <github@thib.top>
2021-12-17 13:04:56 +0000

    hop

--- packages/files-ui/src/Components/Elements/Notifications/NotificationList.tsx
+++ packages/files-ui/src/Components/Elements/Notifications/NotificationList.tsx
@@ -4,6 +4,9 @@ import { createStyles, ITheme, makeStyles } from "@chainsafe/common-theme"
 import dayjs from "dayjs"
 import relativeTime from "dayjs/plugin/relativeTime"
 import { Notification } from "./NotificationsDropdown"
+import { Trans } from "@lingui/macro"
+import { BellIcon } from "@chainsafe/common-components"
+import clsx from "clsx"
 dayjs.extend(relativeTime)
 
 const useStyles = makeStyles(({ palette, constants }: ITheme) =>
@@ -14,16 +17,26 @@ const useStyles = makeStyles(({ palette, constants }: ITheme) =>
       alignItems: "center",
       cursor: "pointer",
       backgroundColor: "initial",
+      "&.empty": {
+        display: "flex",
+        flexDirection: "column",
+        padding: `${constants.generalUnit * 3 }px ${constants.generalUnit * 1.5}px`,
+        color: palette.additional["gray"][7]
+      },
       "&:hover": {
         backgroundColor: palette.additional["gray"][3]
       },
-      "svg": {
+      "& svg": {
         fill: palette.additional["gray"][9]
       },
       borderBottom: `1px solid ${palette.additional["gray"][5]}`,
-      ":last-child": {
+      "&:last-child": {
         border: "none"
       }
+    },
+    icon: {
+      transition: "none",
+      marginBottom: constants.generalUnit
     },
     notificationTitle: {
       color: palette.additional["gray"][9],
@@ -53,6 +66,14 @@ const NotificationList = ({ notifications }: INotificationListProps) => {
       className={classes.scrollContent}
     >
       <div>
+        {notifications.length === 0 && (
+          <div className={clsx(classes.notificationBody, "empty")}>
+            <BellIcon className={classes.icon} />
+            <Typography variant="h4" >
+              <Trans>There are no notifications!</Trans>
+            </Typography>
+          </div>
+        )}
         {notifications.map((n, i) => (
           <div
             key={i}
