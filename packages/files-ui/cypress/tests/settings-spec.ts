bd395146399b80e5a0c8def13a6c41ef3afb9175
Tanmoy Basak Anjan <tanmoy3399@gmail.com>
2021-12-21 01:33:33 +0600

    tests in progress

--- packages/files-ui/cypress/tests/settings-spec.ts
+++ packages/files-ui/cypress/tests/settings-spec.ts
@@ -6,19 +6,53 @@ describe("Settings", () => {
 
   context("desktop", () => {
     beforeEach(() => {
-      cy.web3Login()
+      cy.web3Login({ withNewUser: true, saveBrowser: false })
     })
 
-    it("can navigate to the settings profile page", () => {
-      navigationMenu.settingsNavButton().click()
-      settingsPage.profileTabHeader().should("be.visible")
-      cy.url().should("include", "/settings")
-      settingsPage.profileTabButton().click()
-      cy.url().should("include", "/settings/profile")
-      settingsPage.profileTabHeader().should("be.visible")
-      settingsPage.securityTabButton().click()
-      cy.url().should("include", "/settings/security")
-      settingsPage.securityTabHeader().should("be.visible")
+    describe("settings profile page", () => {
+      beforeEach(() => {
+        navigationMenu.settingsNavButton().click()
+        settingsPage.profileTabHeader().should("be.visible")
+      })
+
+      it("can navigate to settings profile page", () => {
+        cy.url().should("include", "/settings")
+        settingsPage.profileTabButton().click()
+        cy.url().should("include", "/settings/profile")
+        settingsPage.profileTabHeader().should("be.visible")
+      })
+
+      it("save changes button should be disabled without first and last name", () => {
+        settingsPage.firstNameInput().clear()
+        settingsPage.lastNameInput().clear()
+
+        settingsPage.saveChangesButton().should("be.disabled")
+      })
+
+      it("can add/edit firstname and lastname", () => {
+        const newFirstName = "test first name"
+        const newLastName = "test last name"
+
+        settingsPage.firstNameInput().type(newFirstName)
+        settingsPage.lastNameInput().type(`${newLastName}{enter}`)
+
+        settingsPage.firstNameInput().should("have.value", newFirstName)
+        settingsPage.lastNameInput().should("have.value", newLastName)
+
+        settingsPage.saveChangesButton().safeClick()
+        settingsPage.firstNameInput().should("have.value", newFirstName)
+        settingsPage.lastNameInput().should("have.value", newLastName)
+      })
+    })
+
+    describe("settings security page", () => {
+      it("can navigate to settings security page", () => {
+        navigationMenu.settingsNavButton().click()
+        cy.url().should("include", "/settings")
+        settingsPage.securityTabButton().click()
+        cy.url().should("include", "/settings/security")
+        settingsPage.securityTabHeader().should("be.visible")
+      })
     })
   })
 
