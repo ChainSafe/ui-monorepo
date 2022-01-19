7cb726cc0f3c7e446b134a1031d26e181cd5ad57
Andrew Snaith <asnaith@users.noreply.github.com>
2022-01-19 11:13:57 -0800

    add ui tests for add / update / remove card (#1843)

---
+++ packages/files-ui/cypress/fixtures/cardData.ts
@@ -0,0 +1,6 @@
+export const visaNumber = "4242424242424242"
+export const visaCvc = "123"
+export const visaExpiry = "12/30"
+export const mastercardNumber = "5555555555554444"
+export const mastercardCvc = "456"
+export const mastercardExpiry = "01/31"
