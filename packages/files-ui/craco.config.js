3e09d90bc8e5c49d5f8e6a01dfdfdde6c22495e7
Michael Yankelev <myankelev@gmail.com>
2022-01-25 17:27:04 +0200

    testing posthog

--- packages/files-ui/craco.config.js
+++ packages/files-ui/craco.config.js
@@ -24,7 +24,7 @@ module.exports = {
                 warnings: false,
                 comparisons: false,
                 inline: 2,
-                drop_console: true
+                drop_console: false
               },
               mangle: {
                 safari10: true
