ae22cdf484c3c40e0a7d2dca7ce6a66c4fc50bf1
Andrew Snaith <andrew@chainsafe.io>
2022-01-24 21:23:04 -0800

    fix mistake

--- packages/files-ui/cypress/support/page-objects/modals/billing/confirmPlanChangeModal.ts
+++ packages/files-ui/cypress/support/page-objects/modals/billing/confirmPlanChangeModal.ts
@@ -3,19 +3,19 @@ export const confirmPlanChangeModal = {
 
   confirmPlanHeader: () => cy.get("[data-cy=header-confirm-change]"),
 
-  selectedPlanLabel: () => cy.get("[data-cy=link-edit-plan"),
-  editPlanLink: () => cy.get("[data-cy=link-edit-plan"),
-  featuresLabel: () => cy.get("[data-cy=label-features-title"),
-  featuresSummaryLabel: () => cy.get("[data-cy=label-features-summary"),
+  selectedPlanLabel: () => cy.get("[data-cy=link-edit-plan]"),
+  editPlanLink: () => cy.get("[data-cy=link-edit-plan]"),
+  featuresLabel: () => cy.get("[data-cy=label-features-title]"),
+  featuresSummaryLabel: () => cy.get("[data-cy=label-features-summary]"),
 
-  selectedPaymentMethodLabel: () => cy.get("[data-cy=label-selected-payment-method"),
-  cardNumberLabel: () => cy.get("[data-cy=selected-card-number"),
-  editPaymentMethodLink: () => cy.get("[data-cy=link-edit-payment-method"),
-  billingLabel: () => cy.get("[data-cy=label-billing-title"),
-  billingStartDate: () => cy.get("[data-cy=label-billing-start-date"),
-  totalLabel: () => cy.get("[data-cy=label-total-title"),
-  totalPriceLabel: () => cy.get("[data-cy=label-total-price"),
-  changePlanErrorLabel: () => cy.get("[data-cy=label-change-plan-error"),
-  goBackButton: () => cy.get("[data-testid=button-go-back-to-payment-method"),
-  confirmPlanChangeButton: () => cy.get("[data-testid=button-confirm-plan-change")
+  selectedPaymentMethodLabel: () => cy.get("[data-cy=label-selected-payment-method]"),
+  cardNumberLabel: () => cy.get("[data-cy=label-selected-card-number]"),
+  editPaymentMethodLink: () => cy.get("[data-cy=link-edit-payment-method]"),
+  billingLabel: () => cy.get("[data-cy=label-billing-title]"),
+  billingStartDate: () => cy.get("[data-cy=label-billing-start-date]"),
+  totalLabel: () => cy.get("[data-cy=label-total-title]"),
+  totalPriceLabel: () => cy.get("[data-cy=label-total-price]"),
+  changePlanErrorLabel: () => cy.get("[data-cy=label-change-plan-error]"),
+  goBackButton: () => cy.get("[data-testid=button-go-back-to-payment-method]"),
+  confirmPlanChangeButton: () => cy.get("[data-testid=button-confirm-plan-change]")
 }
