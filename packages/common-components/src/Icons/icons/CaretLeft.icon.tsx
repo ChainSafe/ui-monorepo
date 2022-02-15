314022473da4a39a3e2e88b19bedb51cbcb5c287
Tanmoy Basak Anjan <tanmoy3399@gmail.com>
2022-02-16 03:27:37 +0600

    Settings back icon in mobile view (#1946)
    * setting nav
    
    * colors
    
    * conditional arrow

---
+++ packages/common-components/src/Icons/icons/CaretLeft.icon.tsx
@@ -0,0 +1,7 @@
+import * as React from "react"
+import createSvgIcon from "../createSvgIcon"
+import { ReactComponent as CaretLeftSvg } from "../svgs/caret-left.svg"
+
+export { CaretLeftSvg }
+
+export default createSvgIcon(<CaretLeftSvg />)
