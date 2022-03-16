19d0100e2deddecb3f38b37ac873e3b3d3171f29
Andrew Snaith <andrew@chainsafe.io>
2022-03-15 23:00:43 -0700

    add test for crypto invoice notification

--- packages/files-ui/cypress/tests/notifications-spec.ts
+++ packages/files-ui/cypress/tests/notifications-spec.ts
@@ -1,6 +1,11 @@
+import { homePage } from "../support/page-objects/homePage"
 import { navigationMenu } from "../support/page-objects/navigationMenu"
 import { settingsPage } from "../support/page-objects/settingsPage"
-import { homePage } from "../support/page-objects/homePage"
+import { selectPlanModal } from "../support/page-objects/modals/billing/selectPlanModal"
+import { planDetailsModal } from "../support/page-objects/modals/billing/planDetailsModal"
+import { selectPaymentMethodModal } from "../support/page-objects/modals/billing/selectPaymentMethodModal"
+import { planChangeConfirmationModal } from "../support/page-objects/modals/billing/planChangeConfirmationModal"
+import { cryptoPaymentModal } from "../support/page-objects/modals/billing/cryptoPaymentModal"
 
 describe("Notifications", () => {
   beforeEach(() => {
@@ -10,7 +15,7 @@ describe("Notifications", () => {
   })
   context("desktop", () => {
 
-    it("can see a notification if card is expiring soon", () => {
+    it("can see and interact with a notification when a card is expiring soon", () => {
       cy.web3Login({ deleteCreditCard: true, resetToFreePlan: true })
 
       // upgrade subscription with a card expiring this month
@@ -35,6 +40,44 @@ describe("Notifications", () => {
       settingsPage.notificationContainer().should("not.be.visible")
 
       // navigate away and return to plan page from notification
+      navigationMenu.homeNavButton().click()
+      cy.url().should("include", "/drive")
+      homePage.notificationButton().click()
+      homePage.notificationContainer().click()
+      cy.url().should("include", "/settings/plan")
+    })
+
+    it("can see and interact with a notification when crypto invoice is open", () => {
+      cy.web3Login({ deleteCreditCard: true, resetToFreePlan: true })
+      navigationMenu.settingsNavButton().click()
+
+      // initiate crypto payment then exit upgrade flow
+      settingsPage.subscriptionTabButton().click()
+      settingsPage.changePlanButton().click()
+      selectPlanModal.createPlanCypressAliases()
+      cy.get("@filesProBox").parent().within(() => {
+        selectPlanModal.selectPlanButton().click()
+      })
+      planDetailsModal.durationToggleSwitch().click()
+      planDetailsModal.selectThisPlanButton().click()
+      selectPaymentMethodModal.cryptoRadioInput()
+        .should("be.visible")
+        .click()
+      selectPaymentMethodModal.selectPaymentButton().click()
+      planChangeConfirmationModal.confirmPlanChangeButton().click()
+      cryptoPaymentModal.closeButton().click()
+
+      // access and inspect notification menu
+      settingsPage.notificationButton().click()
+      settingsPage.notificationContainer().should("have.length", 1)
+
+      // ensure individual notification has title and date
+      settingsPage.notificationContainer().within(() => {
+        settingsPage.notificationTitle().should("be.visible")
+        settingsPage.notificationTime().should("be.visible")
+      })
+
+      // navigate away and return to the plan page from notification
       navigationMenu.homeNavButton().click()
       cy.url().should("include", "/drive")
       homePage.notificationButton().click()
