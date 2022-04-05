9e7b5877d54fac5db6c7e94085cb2ee07cc31136
Andrew Snaith <asnaith@users.noreply.github.com>
2022-04-05 09:32:11 -0700

    add new ui tests for cid searching (#2073)

--- packages/storage-ui/cypress/support/page-objects/cidsPage.ts
+++ packages/storage-ui/cypress/support/page-objects/cidsPage.ts
@@ -1,7 +1,7 @@
 // Only add things here that could be applicable to the cids page
 
 import { basePage } from "./basePage"
-import { testCid } from "../../fixtures/storageTestData"
+import { testCid, testCidName } from "../../fixtures/storageTestData"
 import { addCidModal } from "./modals/addCidModal"
 
 export const cidsPage = {
@@ -12,21 +12,27 @@ export const cidsPage = {
   pinButton: () => cy.get("[data-cy=button-pin-cid]"),
 
   // cid browser row elements
+  searchCidInput: () => cy.get("[data-testid=input-search-cid]"),
   cidTableHeader: () => cy.get("[data-cy=table-header-cid]"),
   createdTableHeader: () => cy.get("[data-cy=table-header-created]"),
   sizeTableHeader: () => cy.get("[data-cy=table-header-size]"),
   statusTableHeader: () => cy.get("[data-cy=table-header-status]"),
   cidItemRow: () => cy.get("[data-cy=row-cid-item]", { timeout: 20000 }),
   cidNameCell: () => cy.get("[data-cy=cell-pin-name]"),
+  cidCell: () => cy.get("[data-cy=cell-pin-cid]"),
   cidRowKebabButton: () => cy.get("[data-testid=dropdown-title-cid-kebab]"),
 
   // menu elements
   unpinMenuOption: () => cy.get("[data-cy=menu-unpin]"),
 
   // helpers and convenience functions
-  addPinnedCid() {
+  addPinnedCid(cidDetails?: {name?: string; cid?: string}) {
+    const name = cidDetails?.name ? cidDetails.name : testCidName
+    const cid = cidDetails?.cid ? cidDetails.cid : testCid
+
     this.pinButton().click()
-    addCidModal.cidInput().type(testCid)
+    addCidModal.nameInput().type(name)
+    addCidModal.cidInput().type(cid)
     addCidModal.pinSubmitButton().click()
     this.cidItemRow().should("have.length", 1)
   }
