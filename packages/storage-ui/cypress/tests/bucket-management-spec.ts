850f33630516903c9f266f44b4641b1987fcc26b
Andrew Snaith <andrew@chainsafe.io>
2022-04-11 19:26:19 -0700

    update test spec for creating both types of bucket

--- packages/storage-ui/cypress/tests/bucket-management-spec.ts
+++ packages/storage-ui/cypress/tests/bucket-management-spec.ts
@@ -1,36 +1,53 @@
 import { bucketsPage } from "../support/page-objects/bucketsPage"
 import { bucketName } from "../fixtures/storageTestData"
-
+import { createBucketModal } from "../support/page-objects/modals/createBucketModal"
 import { navigationMenu } from "../support/page-objects/navigationMenu"
 
 describe("Bucket management", () => {
 
   context("desktop", () => {
 
-    it("can create a bucket", () => {
+    it("can create and delete a chainsafe bucket", () => {
       cy.web3Login({ clearPins: true, deleteFpsBuckets: true })
-
-      // create a bucket and see it in the bucket table
       navigationMenu.bucketsNavButton().click()
-      bucketsPage.createBucketButton().click()
-      bucketsPage.bucketNameInput().type(bucketName)
-      bucketsPage.createBucketSubmitButton().safeClick()
-      bucketsPage.bucketItemRow().should("have.length", 1)
-      bucketsPage.bucketItemName().should("have.text", bucketName)
 
       // open create bucket modal and cancel it
       bucketsPage.createBucketButton().click()
-      bucketsPage.createBucketCancelButton().click()
-      bucketsPage.createBucketForm().should("not.exist")
+      createBucketModal.cancelButton().click()
+      createBucketModal.body().should("not.exist")
+
+      // create a bucket and see it in the bucket table
+      bucketsPage.createBucketButton().click()
+      createBucketModal.body().should("be.visible")
+      createBucketModal.bucketNameInput().type(bucketName)
+      createBucketModal.chainsafeRadioInput().click()
+      createBucketModal.submitButton().click()
+      bucketsPage.bucketItemRow().should("have.length", 1)
+      bucketsPage.bucketItemName().should("have.text", bucketName)
+      bucketsPage.bucketFileSystemType().should("have.text", "Chainsafe")
+
+      // delete chainsafe bucket
+      bucketsPage.bucketRowKebabButton().first().click()
+      bucketsPage.deleteBucketMenuOption().first().click()
+      bucketsPage.bucketItemRow().should("not.exist")
+      bucketsPage.bucketItemName().should("not.exist")
     })
 
-    it("can delete a bucket", () => {
+    it("can create and delete an ipfs bucket", () => {
       cy.web3Login({ clearPins: true, deleteFpsBuckets: true })
-
-      // delete a bucket and ensure its row is removed
       navigationMenu.bucketsNavButton().click()
-      // creating a bucket with a unique name
-      bucketsPage.createBucket(`${bucketName}_${Date.now()}`)
+
+      // create a bucket and see it in the bucket table
+      bucketsPage.createBucketButton().click()
+      createBucketModal.body().should("be.visible")
+      createBucketModal.bucketNameInput().type(bucketName)
+      createBucketModal.ipfsRadioInput().click()
+      createBucketModal.submitButton().click()
+      bucketsPage.bucketItemRow().should("have.length", 1)
+      bucketsPage.bucketItemName().should("have.text", bucketName)
+      bucketsPage.bucketFileSystemType().should("have.text", "IPFS MFS")
+
+      // delete ipfs bucket
       bucketsPage.bucketRowKebabButton().first().click()
       bucketsPage.deleteBucketMenuOption().first().click()
       bucketsPage.bucketItemRow().should("not.exist")
