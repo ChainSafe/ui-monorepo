5ddd5226252dc5c56aa00adcbf4caae6ee5b58d8
Michael Yankelev <myankelev@gmail.com>
2022-04-28 17:51:52 +0200

    more recursion for arrays

--- packages/storage-ui/src/Components/Pages/UploadNFT.tsx
+++ packages/storage-ui/src/Components/Pages/UploadNFT.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react"
+import React, { ReactNode, useCallback, useState } from "react"
 import { Button, Divider, FileInput, FormikTextInput, SelectInput, TextInput, Typography } from "@chainsafe/common-components"
 import { makeStyles, createStyles, useThemeSwitcher, ITheme } from "@chainsafe/common-theme"
 import { t, Trans } from "@lingui/macro"
@@ -8,6 +8,7 @@ import { FieldArray, Form, Formik, FormikProvider, useFormik } from "formik"
 import * as yup from "yup"
 import { useStorageApi } from "../../Contexts/StorageApiContext"
 import FormikImageInput from "../Elements/FormikImageInput"
+import { type } from "os"
 
 const useStyles = makeStyles(({ constants, breakpoints }: ITheme) =>
   createStyles({
@@ -33,23 +34,85 @@ const useStyles = makeStyles(({ constants, breakpoints }: ITheme) =>
   })
 )
 
+const primitives = ["boolean", "string", "number"]
+
+const generateFormFields = (obj: any, namespace?: string): ReactNode | (ReactNode | undefined)[] => {
+  if (!!namespace && primitives.includes(typeof obj)) {
+    return <FormikTextInput
+      name={namespace || ""}
+      label={namespace} 
+      key={namespace} />
+  }
+
+  if (!!namespace && (obj === undefined || (typeof obj === "object" && (obj instanceof File || obj instanceof Blob)))) {
+    return <FileInput
+      name={namespace || ""}
+      label={namespace || ""}
+      onFileNumberChange={() => undefined}
+      moreFilesLabel={""}
+      maxFiles={1}
+      key={namespace} />
+  }
+
+  if (typeof obj === "object") {
+    // Map through each of the child properties and create fields to handle inputs for these
+    return Object.keys(obj).map((key, index) => {
+      // Primitive fields should be instantiated to an empty string
+      if ((primitives.includes(typeof obj[key])) && obj[key] !== undefined)
+        return <FormikTextInput
+          key={index}
+          name={`${namespace}.${key}`}
+          label={key} />
+
+      // File fields should be instantiated to undefined
+      if (obj[key] === undefined || (typeof obj[key] === "object" && (obj[key] instanceof File || obj[key] instanceof Blob)))
+        return <FileInput
+          name={`${namespace}.${key}`}
+          key={key}
+          label={key}
+          onFileNumberChange={() => undefined}
+          moreFilesLabel={""}
+          maxFiles={1} />
+
+      // Handle arrays by recursively generating a FieldArray and 
+      if (typeof obj[key] === "object" && Array.isArray(obj[key]))
+        return <FieldArray
+          name={`${namespace}.${key}`}
+          key={key}>
+          {({ push }) => <>
+            <Typography>{key}</Typography>
+            {obj[key].map((item: any, index: number) => generateFormFields(item, `${namespace}.${key}[${index}]`))}
+            <Button onClick={() => push("")}>Add Value</Button>
+          </>
+          }
+        </FieldArray>
+
+      if (typeof obj[key] === "object") {
+        return generateFormFields(obj[key], `${namespace}.${key}`)
+      }
+
+      return undefined
+    })
+  }
+}
+
 const CreateNFTPage: React.FC = () => {
   const { desktop } = useThemeSwitcher()
   const classes = useStyles()
   usePageTrack()
 
-  const [initialValues, setInitialValues] = useState<{ [key: string]: string | number | File | Array<any> | any }>({
+  const initialValues: { [key: string]: any } = {
     image: undefined,
     name: "",
     decimals: "",
     description: "",
     properties: {}
-  })
+  }
 
   const formikProps = useFormik({
     initialValues: initialValues,
     // validationSchema={validationSchema}
-    onSubmit: async(val) => {
+    onSubmit: async (val) => {
       // const result = await storageApiClient.uploadNFT(val)
       console.log(val)
     },
@@ -61,6 +124,7 @@ const CreateNFTPage: React.FC = () => {
 
 
   const [newFieldName, setNewFieldName] = useState("")
+  const [newFieldType, setNewFieldType] = useState<"value" | "array" | "file" | "object">("value")
 
   // const validationSchema = yup.object().shape({
   //   name: yup.string().required("Name is required"),
@@ -105,23 +169,24 @@ const CreateNFTPage: React.FC = () => {
           <FormikTextInput name='name' />
           <FormikTextInput name='description' />
           <FormikTextInput name='decimals' />
-          <FormikImageInput name='image'/>
-          {Object.keys(formikProps.values.properties).map(k => {
-          // Primitive fields should be instantiated to an empty string
-          // File fields should be instantiated to undefined
-            if (typeof (formikProps.values.properties[k]) === "string" || typeof (formikProps.values.properties[k]) === "number")
+          <FormikImageInput name='image' />
+          {generateFormFields(formikProps.values.properties, "properties")
+          /* {Object.keys(formikProps.values.properties).map((key, index) => {
+            // Primitive fields should be instantiated to an empty string
+            // File fields should be instantiated to undefined
+            if (typeof (formikProps.values.properties[key]) === "string" || typeof (formikProps.values.properties[key]) === "number")
               return <FormikTextInput
-                name={`properties.${k}`}
-                label={k} />
-            // if (typeof initialValues.properties[k] === "object" && Array.isArray(initialValues.properties[k])) return <FieldArray name={`properties.${k}`} />
-            // if ((typeof initialValues.properties[k] === "object" && initialValues.properties[k] instanceof File) || typeof initialValues.properties[k] === "undefined")
-            //   return <FileInput
-            //     name={k}
-            //     label={k}
-            //     maxFiles={1}
-            //     moreFilesLabel="" />
-            return undefined
-          })}
+                key={index}
+                name={`properties.${key}`}
+                label={key} />
+              // if (typeof initialValues.properties[k] === "object" && Array.isArray(initialValues.properties[k])) return <FieldArray name={`properties.${k}`} />
+              // if ((typeof initialValues.properties[k] === "object" && initialValues.properties[k] instanceof File) || typeof initialValues.properties[k] === "undefined")
+              //   return <FileInput
+              //     name={k}
+              //     label={k}
+              //     maxFiles={1}
+              //     moreFilesLabel="" />
+            return undefined */}
           <Button type="submit">Upload</Button>
         </Form>
       </FormikProvider>
@@ -142,14 +207,21 @@ const CreateNFTPage: React.FC = () => {
           label: "File",
           value: "file"
         }]}
-        onChange={() => undefined} />
+        onChange={setNewFieldType}
+        value={newFieldType} />
       <Button onClick={
         () => {
           formikProps.setValues({
             ...formikProps.values,
             properties: {
               ...formikProps.values.properties,
-              [newFieldName]: ""
+              [newFieldName]: (newFieldType === "array")
+                ? []
+                : (newFieldType === "file")
+                  ? undefined
+                  : (newFieldType === "object")
+                    ? {}
+                    : ""
             }
           })
         }}>
