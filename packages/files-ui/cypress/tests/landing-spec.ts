4889a825266aa7edb247e87b876a707c0c561001
Juan Manuel Spoleti <juan@chainsafe.io>
2022-05-03 14:53:22 -0300

    update elements name in authenticationPage from button to link

--- packages/files-ui/cypress/tests/landing-spec.ts
+++ packages/files-ui/cypress/tests/landing-spec.ts
@@ -8,17 +8,17 @@ describe("Landing", () => {
   context("desktop", () => {
 
     it("can navigate to privacy policy page", () => {
-      authenticationPage.privacyPolicyButton().invoke('removeAttr', 'target').click()
+      authenticationPage.privacyPolicyLink().invoke('removeAttr', 'target').click()
       cy.url().should("include", "/privacy-policy")
     })
 
     it("can navigate to terms & conditions page", () => {
-      authenticationPage.termsAndConditionsButton().invoke('removeAttr', 'target').click()
+      authenticationPage.termsAndConditionsLink().invoke('removeAttr', 'target').click()
       cy.url().should("include", "/terms-of-service")
     })
 
     it("can navigate to ChainSafe.io from 'Learn more about Chainsafe'", () => {
-      authenticationPage.learnMoreAboutChainsafeButton().invoke('removeAttr', 'target').click()
+      authenticationPage.learnMoreAboutChainsafeLink().invoke('removeAttr', 'target').click()
       cy.url().should("eq", "https://chainsafe.io/")
     })
   })
