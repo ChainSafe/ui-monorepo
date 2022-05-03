4c5fec8a9f819594d2436f834b0a5a0f30934bce
Andrew Snaith <andrew@chainsafe.io>
2022-05-03 15:16:08 -0700

    Remove and decrease unnecessary waits

--- packages/storage-ui/cypress/support/page-objects/modals/fileUploadModal.ts
+++ packages/storage-ui/cypress/support/page-objects/modals/fileUploadModal.ts
@@ -1,9 +1,9 @@
 export const fileUploadModal = {
-  body: () => cy.get("[data-cy=form-upload-file] input", { timeout: 20000 }),
+  body: () => cy.get("[data-cy=form-upload-file] input", { timeout: 10000 }),
   cancelButton: () => cy.get("[data-testid=button-cancel-upload]"),
   fileList: () => cy.get("[data-testid=list-fileUpload] li"),
   removeFileButton: () => cy.get("[data-testid=button-remove-from-file-list]"),
-  uploadButton: () => cy.get("[data-testid=button-start-upload]", { timeout: 10000 }),
+  uploadButton: () => cy.get("[data-testid=button-start-upload]"),
   uploadDropzone : () => cy.get("[data-testid=input-file-dropzone-fileUpload]"),
   errorLabel: () => cy.get("[data-testid=meta-error-message-fileUpload]")
 }
