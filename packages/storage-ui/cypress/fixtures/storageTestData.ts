424e91c85f47943a89588f64f7f5b660866eab99
Andrew Snaith <andrew@chainsafe.io>
2022-05-03 20:03:22 -0700

    simplify

--- packages/storage-ui/cypress/fixtures/storageTestData.ts
+++ packages/storage-ui/cypress/fixtures/storageTestData.ts
@@ -1,5 +1,5 @@
-export const chainSafeBucketName = `cs bucket ${ new Date().getTime().toString() }`
-export const ipfsBucketName = `ipfs bucket ${ new Date().getTime().toString() }`
+export const chainSafeBucketName = `cs bucket ${Date.now()}`
+export const ipfsBucketName = `ipfs bucket ${Date.now()}`
 export const testCid = "QmZEE7Ymh2mRMURLnFLipJTovb44AoUYDzx7aipYZwvxX5"
 export const testCidAlternative = "QmSMNExVSNKwWNeXL3o6a2og13441g1eUqLgTeuCSDvN2W"
 export const testCidName = "cute cat"
