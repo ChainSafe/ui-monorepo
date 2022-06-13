106b0dc365dd33a5a4da6848b8bcc41c18658756
Thibaut Sardan <github@thib.top>
2022-06-13 12:56:17 +0100

    remove duplicate testid for dialog buttons

--- packages/storage-ui/cypress/support/page-objects/modals/deleteBucketModal.ts
+++ packages/storage-ui/cypress/support/page-objects/modals/deleteBucketModal.ts
@@ -1,5 +1,5 @@
 export const deleteBucketModal = {
   body: () => cy.get("[data-testid=modal-container-bucket-deletion]"),
-  cancelButton: () => cy.get("[data-testid=button-bucket-cancel-deletion]"),
-  confirmButton: () => cy.get("[data-testid=button-bucket-confirm-deletion]", { timeout: 10000 })
+  cancelButton: () => cy.get("[data-testid=button-reject-bucket-deletion]"),
+  confirmButton: () => cy.get("[data-testid=button-confirm-bucket-deletion]", { timeout: 10000 })
 }
