b9c91057d7bdaf0800941a7e011e4b3566317fcd
Juan Manuel Spoleti <juan@chainsafe.io>
2022-06-29 10:06:11 -0300

    Revert "sort Buckets table test"
    This reverts commit e5cfb6054b021ac602ccfc3aba8d7c86caddaec3.

--- packages/storage-ui/cypress/support/page-objects/bucketsPage.ts
+++ packages/storage-ui/cypress/support/page-objects/bucketsPage.ts
@@ -1,6 +1,5 @@
 // Only add things here that could be applicable to the bucket page
 
-import { FILE_SYSTEM_TYPES } from "../utils/TestConstants"
 import { basePage } from "./basePage"
 import { createBucketModal } from "./modals/createBucketModal"
 
@@ -14,7 +13,6 @@ export const bucketsPage = {
   // bucket browser row elements
   bucketItemRow: () => cy.get("[data-cy=row-bucket-item]"),
   nameTableHeader: () => cy.get("[data-cy=table-header-name]"),
-  fileSystemTableHeader: () => cy.get("[data-cy=table-header-file-system]"),
   sizeTableHeader: () => cy.get("[data-cy=table-header-size]"),
   bucketItemName: () => cy.get("[data-cy=cell-bucket-name]"),
   bucketFileSystemType: () => cy.get("[data-cy=cell-file-system-type]"),
@@ -24,19 +22,11 @@ export const bucketsPage = {
   deleteBucketMenuOption: () => cy.get("[data-cy=menu-delete-bucket]"),
 
   // helpers and convenience functions
-  createBucket(bucketName: string, bucketType: FILE_SYSTEM_TYPES) {
+  createBucket(bucketName: string) {
     this.createBucketButton().click()
     createBucketModal.body().should("be.visible")
     createBucketModal.bucketNameInput().type(bucketName)
-    switch (bucketType) {
-      case FILE_SYSTEM_TYPES.IPFS:
-        createBucketModal.ipfsRadioInput().click()
-        break
-      case FILE_SYSTEM_TYPES.CHAINSAFE:
-        createBucketModal.chainsafeRadioInput().click()
-        break
-    }
-    createBucketModal.submitButton().click()
+    createBucketModal.submitButton().safeClick()
     createBucketModal.body().should("not.exist")
   }
 }
