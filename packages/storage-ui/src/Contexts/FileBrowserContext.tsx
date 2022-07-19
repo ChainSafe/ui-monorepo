dfa01ea26f8b7c5e2fb55be8bd0836f1e42aad9c
Tanmoy Basak Anjan <tanmoy3399@gmail.com>
2022-07-19 22:39:12 +0600

    type cleanings

--- packages/storage-ui/src/Contexts/FileBrowserContext.tsx
+++ packages/storage-ui/src/Contexts/FileBrowserContext.tsx
@@ -31,8 +31,8 @@ interface FileBrowserContext extends IFileBrowserModuleProps {
   currentPath: string
   loadingCurrentPath: boolean
   sourceFiles: FileSystemItem[]
-  crumbs?: Crumb[] | undefined
-  moduleRootPath?: string | undefined
+  crumbs?: Crumb[]
+  moduleRootPath?: string
   getPath?: (cid: string) => string
   isSearch?: boolean
   withSurvey?: boolean
