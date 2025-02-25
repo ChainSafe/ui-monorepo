6b68cb3413dadd5400046d7fa1c4ee9bf9303898
irubido <ivan.rubido@gmail.com>
2025-02-25 11:08:04 +0100

    closing banner

---
+++ packages/files-ui/src/Components/Elements/Banner.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Box, Typography } from "@material-ui/core";
+
+const Banner = () => {
+  return (
+    <Box 
+      width="100%" 
+      padding={2} 
+      bgcolor="#ffc4c4"
+      border={1}
+      borderColor="grey.300"
+      boxShadow={1}
+      textAlign="center"
+      margin={0}
+    >
+      <Typography variant="body1" >
+        To our valued customers, ChainSafe (Storage/Files) will be sunset on March 10th, 2025. Please retrieve all of your files on the platform before this date to avoid loosing access. Feel free to send an email to colin@chainsafe.io if you have any questions or concerns.
+      </Typography>
+    </Box>
+  );
+};
+
+export default Banner;
